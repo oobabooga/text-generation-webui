ef4e610d37be8fb77dd46403cc68290268c7670e
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-01-07 23:01:39 -0300

    Re-enable the progress bar in notebook mode

--- server.py
+++ server.py
@@ -142,13 +142,13 @@ if args.notebook:
 
         with gr.Row():
             with gr.Column():
-                temp_slider = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label='Temperature', value=0.7)
                 length_slider = gr.Slider(minimum=1, maximum=2000, step=1, label='max_length', value=200)
+                temp_slider = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label='Temperature', value=0.7)
             with gr.Column():
                 preset_menu = gr.Dropdown(choices=available_presets, value="NovelAI-Sphinx Moth", label='Preset')
                 model_menu = gr.Dropdown(choices=available_models, value=model_name, label='Model')
 
-        btn.click(generate_reply, [textbox, temp_slider, length_slider, preset_menu, model_menu], [textbox, markdown, html], show_progress=False)
+        btn.click(generate_reply, [textbox, temp_slider, length_slider, preset_menu, model_menu], [textbox, markdown, html], show_progress=True)
 elif args.chat:
     history = []
 
