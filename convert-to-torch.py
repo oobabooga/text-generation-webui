18ae08ef9176b9f9e460d2e8a396127cc5f2422a
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-01-10 23:41:35 -0300

    Remove T5 support

--- convert-to-torch.py
+++ convert-to-torch.py
@@ -7,7 +7,7 @@ python convert-to-torch.py models/opt-1.3b
 The output will be written to torch-dumps/name-of-the-model.pt
 '''
  
-from transformers import AutoModelForCausalLM, T5ForConditionalGeneration
+from transformers import AutoModelForCausalLM
 import torch
 from sys import argv
 from pathlib import Path
@@ -16,10 +16,7 @@ path = Path(argv[1])
 model_name = path.name
 
 print(f"Loading {model_name}...")
-if model_name in ['flan-t5', 't5-large']:
-    model = T5ForConditionalGeneration.from_pretrained(path).cuda()
-else:
-    model = AutoModelForCausalLM.from_pretrained(path, low_cpu_mem_usage=True, torch_dtype=torch.float16).cuda()
+model = AutoModelForCausalLM.from_pretrained(path, low_cpu_mem_usage=True, torch_dtype=torch.float16).cuda()
 print("Model loaded.")
 
 print(f"Saving to torch-dumps/{model_name}.pt")
