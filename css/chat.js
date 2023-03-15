87b84d227555894c1f53168c4774974ebffef46c
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-03-15 16:39:59 -0300

    CSS fix

--- css/chat.js
+++ css/chat.js
@@ -1,2 +1,6 @@
-document.getElementById("main").childNodes[0].style = "max-width: 800px; margin-left: auto; margin-right: auto";
-document.getElementById("extensions").style = "max-width: 800px; margin-left: auto; margin-right: auto";
+document.getElementById("main").style.setProperty("max-width", "800px");
+document.getElementById("main").style.setProperty("margin-left", "auto");
+document.getElementById("main").style.setProperty("margin-right", "auto");
+document.getElementById("extensions").style.setProperty("max-width", "800px");
+document.getElementById("extensions").style.setProperty("margin-left", "auto");
+document.getElementById("extensions").style.setProperty("margin-right", "auto");
