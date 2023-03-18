7c945cfe8ecb38e41ee57c8884d582e0b453bc8a
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-03-18 10:55:24 -0300

    Don't include PeftModel every time

--- modules/LoRA.py
+++ modules/LoRA.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from peft import PeftModel
-
 import modules.shared as shared
 from modules.models import load_model
 
 
 def add_lora_to_model(lora_name):
+
+    from peft import PeftModel
 
     # Is there a more efficient way of returning to the base model?
     if lora_name == "None":
