859ce5973a4f0cfac43c18ff8660e90688285c86
Dan Kild <dan.kild@speedandfunction.com>
2023-03-23 00:52:33 +0200

    reworked i18n

--- extensions/pyrogram_bot/src/i18n.py
+++ extensions/pyrogram_bot/src/i18n.py
@@ -1,8 +1,31 @@
-from yaml import load as parse
+import yaml
 from os.path import dirname
+
+from typing import Callable, NewType
 
 root_folder = dirname(dirname(__file__))
 
-def get_i18n(lang = 'en'):
-  with open(f"{root_folder}/i18n/{lang}.yml") as file:
-    return parse(file.read())
+def get_i18n(lang = 'ru') -> Callable[[str, dict[str, str]], str]:
+  file_path = f"{root_folder}/i18n/{lang}.yml"
+
+  dict_parsed = {}
+  with open(file_path) as file:
+    file_content = file.read()
+    dict_parsed = yaml.load(file_content, Loader=yaml.FullLoader)
+
+  def t(address: str, replaceble: dict[str, str] = {}) -> str:
+    result = dict_parsed
+
+    for i in address.split('.'):
+      elem = result.get(i, None)
+
+      if not elem:
+        raise KeyError(address)
+
+      result = elem
+      if type(elem) == str:
+        for before, after in replaceble.items():
+          elem = elem.replace(before, str(after))
+        return elem
+
+  return t
