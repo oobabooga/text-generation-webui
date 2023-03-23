9eda83b5aac243d566ec0c436bd806ec8a0c8e75
Dan Kild <dan.kild@speedandfunction.com>
2023-03-23 16:06:31 +0200

    Added Threading events and singleton thread

--- extensions/pyrogram_bot/script.py
+++ extensions/pyrogram_bot/script.py
@@ -1,14 +1,14 @@
-import asyncio
-from threading import Thread
-
-import uvloop
+import asyncio, atexit, threading, uvloop
 
 from .client import Client
 
+stop_event = threading.Event()
+running = threading.Event()
 
 async def pyrogram_main() -> None:
+  running.set()
   app = Client()
-  await app.run()
+  await app.run(stop_event)
 
 def start_bot() -> None:
   uvloop.install()
@@ -16,8 +16,16 @@ def start_bot() -> None:
   asyncio.set_event_loop(loop)
   loop.run_until_complete(pyrogram_main())
 
+def exit_handle():
+  print('\nGracefully stopping pyrogram bot.\n', flush=True)
+  stop_event.set()
+
 def ui() -> None:
-  Thread(target=start_bot, daemon=True).start()
+  if not running.is_set():
+    threading\
+      .Thread(target=start_bot, daemon=True, name="pyrogram_thread")\
+      .start()
+    atexit.register(exit_handle)
 
 if __name__=="__main__":
   start_bot()
