fcb6d9d4c00ccddc037de291cd5fc6a621cd2b14
Dan Kild <dan.kild@speedandfunction.com>
2023-03-23 16:04:04 +0200

    delete get_char

--- extensions/pyrogram_bot/controllers/char.py
+++ extensions/pyrogram_bot/controllers/char.py
@@ -1,7 +1,7 @@
 from modules import shared
 
 from ..client import Client as app
-from ..services.model import get_model, set_model
+from ..services.char import get_chars, set_char
 
 Message = app.types.Message
 filters = app.filters
@@ -10,22 +10,17 @@ filters = app.filters
 async def index(_: app, msg: Message) -> None:
   new_msg = await msg.reply(app.t('message.is_typing'))
 
-  answer = app.t('model.index') + '\n'.join(get_model())
+  answer = app.t('model.index') + '\n'.join(get_chars())
 
   await new_msg.edit(answer)
-  msg.stop_propagation()
-
-@app.on_message(filters.command(["character"]))
-async def get(_: app, msg: Message) -> None:
-  await msg.reply(app.t('character.get', {'/name/': shared.character}))
   msg.stop_propagation()
 
 @app.on_message(filters.command(["set_character"]))
 async def put(_: app, msg: Message) -> None:
   new_msg = await msg.reply(app.t('message.is_typing'))
 
-  status = 'succesful' if set_model(msg.command[1]) else 'failed'
-  answer = app.t(f'model.put.{status}', {'/name/': shared.model_name})
+  status = 'succesful' if set_char(msg.command[1]) else 'failed'
+  answer = app.t(f'model.put.{status}', {'/name/': shared.character})
 
   await new_msg.edit(answer)
   msg.stop_propagation()
