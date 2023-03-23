fb71bf2fd509df7d9f472b039162b61a223a69e6
Dan Kild <dan.kild@speedandfunction.com>
2023-03-23 16:06:06 +0200

    make client to stop by event

--- extensions/pyrogram_bot/client.py
+++ extensions/pyrogram_bot/client.py
@@ -1,5 +1,5 @@
-import asyncio
-from os import getcwd
+import asyncio, sqlite3, threading
+from os import getcwd, remove
 from os.path import dirname, relpath
 
 from modules import shared
@@ -8,7 +8,8 @@ from .services.i18n import get_i18n
 from .services.get_creds import get_creds
 
 path = dirname(relpath(__file__, getcwd()))
-t = get_i18n()
+lang = 'en'
+t = get_i18n(lang)
 
 try:
   from pyrogram import Client, filters, types
@@ -20,6 +21,8 @@ class Client(Client):
   filters = filters
   types = types
   cwd = path
+  lang = lang
+  t = t
 
   def __init__(self, **kwargs) -> Client:
     try:
@@ -31,22 +34,20 @@ class Client(Client):
           "root": f"{path}/controllers"
         }
       }
-      return super()(f"{path}/textgen", **creds)
+      return super().__init__(f"{path}/textgen", **creds)
     except bad_request_400.ApiIdInvalid:
       raise Exception(t('error.credential_file.is_invalid'))
 
-  def start(self) -> None:
-    self.t = t
-    super().start()
+  def change_lang(self, lang='en') -> None:
+    self.lang = lang
+    self.t = get_i18n(lang)
 
-  def change_lang(self) -> None:
-    self.t = get_i18n()
-
-  async def run(self):
+  async def run(self, stop_event: threading.Event):
     async with self:
-      try:
-        await self.start()
-      except ConnectionError:
-        pass
-      while True:
+      await self.start()
+
+      while not stop_event.is_set():
         await asyncio.sleep(1)
+      else:
+        print('stopping', flush=True)
+        await self.stop()
