29bd41d453cc8404b7183af685cdd4b952e96435
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-03-23 01:05:13 -0300

    Fix LoRA in CPU mode

--- modules/LoRA.py
+++ modules/LoRA.py
@@ -18,10 +18,10 @@ def add_lora_to_model(lora_name):
         params = {}
         if shared.args.load_in_8bit:
             params['device_map'] = {'': 0}
-        else:
+        elif not shared.args.cpu:
             params['device_map'] = 'auto' 
             params['dtype'] = shared.model.dtype
             
         shared.model = PeftModel.from_pretrained(shared.model, Path(f"loras/{lora_name}"), **params)
-        if not shared.args.load_in_8bit:
+        if not shared.args.load_in_8bit and not shared.args.cpu:
             shared.model.half()
