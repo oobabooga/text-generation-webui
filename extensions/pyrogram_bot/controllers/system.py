d349d26caedc850818658a2b827c555c85320c65
Dan Kild <dan.kild@speedandfunction.com>
2023-03-23 16:04:24 +0200

    add info endpoint

--- extensions/pyrogram_bot/controllers/system.py
+++ extensions/pyrogram_bot/controllers/system.py
@@ -13,3 +13,19 @@ async def settings(_: app, msg: app.types.Message) -> None:
   print(shared.settings, flush=True)
   await msg.reply(shared.settings)
   msg.stop_propagation()
+
+@app.on_message(app.filters.command(['info']))
+async def settings(_: app, msg: app.types.Message) -> None:
+  t = app.t
+  answer = ''
+  answer_dict = {
+    t('model.is'): shared.model_name,
+    t('character.is'): shared.character,
+    t('lang.is'): t(f'lang.{app.lang}'),
+  }
+
+  for key, value in answer_dict.items():
+    answer += f"{key}: {value}\n"
+
+  await msg.reply(answer)
+  msg.stop_propagation()
