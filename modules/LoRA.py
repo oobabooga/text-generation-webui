9bf6ecf9e2de9b72c3fa62e0e6f5b5e9041825b1
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-03-23 16:49:41 -0300

    Fix LoRA device map (attempt)

--- modules/LoRA.py
+++ modules/LoRA.py
@@ -16,12 +16,15 @@ def add_lora_to_model(lora_name):
         print(f"Adding the LoRA {lora_name} to the model...")
         
         params = {}
-        if shared.args.load_in_8bit:
-            params['device_map'] = {'': 0}
-        elif not shared.args.cpu:
-            params['device_map'] = 'auto' 
+        if not shared.args.cpu:
             params['dtype'] = shared.model.dtype
+            if hasattr(shared.model, "hf_device_map"):
+                params['device_map'] = {"base_model.model."+k: v for k, v in shared.model.hf_device_map.items()}
+            elif shared.args.load_in_8bit:
+                params['device_map'] = {'': 0}
             
         shared.model = PeftModel.from_pretrained(shared.model, Path(f"loras/{lora_name}"), **params)
         if not shared.args.load_in_8bit and not shared.args.cpu:
             shared.model.half()
+            if not hasattr(shared.model, "hf_device_map"):
+                shared.model.cuda()
