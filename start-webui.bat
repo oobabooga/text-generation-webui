bddbc2f89848d4cbc554c8f91a39761955b34b76
jllllll <jacob201ac@yahoo.com>
2023-03-24 18:19:23 -0500

    Update start-webui.bat
    Updated virtual environment handling to use Micromamba.

--- start-webui.bat
+++ start-webui.bat
@@ -2,10 +2,19 @@
 
 @echo Starting the web UI...
 
+cd /D "%~dp0"
+
+set MAMBA_ROOT_PREFIX=%cd%\installer_files\mamba
 set INSTALL_ENV_DIR=%cd%\installer_files\env
-set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%INSTALL_ENV_DIR%\Library\usr\bin;%PATH%
-call conda activate
+
+if not exist "%MAMBA_ROOT_PREFIX%\Scripts\activate.bat" (
+  call "%MAMBA_ROOT_PREFIX%\micromamba.exe" shell hook >nul 2>&1
+)
+call "%MAMBA_ROOT_PREFIX%\condabin\mamba_hook.bat" || ( echo Micromamba hook not found. && goto end )
+call micromamba activate "%INSTALL_ENV_DIR%" || goto end
 cd text-generation-webui
+
 call python server.py --auto-devices --cai-chat
 
+:end
 pause
