2aac1fb9d27ad9a70ca72b43a21bbfea5a955d59
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-03-25 00:28:29 -0300

    Merge main

--- modules/GPTQ_loader.py
+++ modules/GPTQ_loader.py
@@ -37,26 +37,32 @@ def load_quantized(model_name):
 
     path_to_model = Path(f'models/{model_name}')
     found_pts = list(path_to_model.glob("*.pt"))
+    found_safetensors = list(path_to_model.glob("*.safetensors"))
     pt_path = None
 
     if len(found_pts) == 1:
         pt_path = found_pts[0]
+    elif len(found_safetensors) == 1:
+        pt_path = found_safetensors[0]
     else:
         if path_to_model.name.lower().startswith('llama-7b'):
-            pt_model = f'llama-7b-{shared.args.wbits}bit.pt'
+            pt_model = f'llama-7b-{shared.args.gptq_bits}bit'
         elif path_to_model.name.lower().startswith('llama-13b'):
-            pt_model = f'llama-13b-{shared.args.wbits}bit.pt'
+            pt_model = f'llama-13b-{shared.args.gptq_bits}bit'
         elif path_to_model.name.lower().startswith('llama-30b'):
-            pt_model = f'llama-30b-{shared.args.wbits}bit.pt'
+            pt_model = f'llama-30b-{shared.args.gptq_bits}bit'
         elif path_to_model.name.lower().startswith('llama-65b'):
-            pt_model = f'llama-65b-{shared.args.wbits}bit.pt'
+            pt_model = f'llama-65b-{shared.args.gptq_bits}bit'
         else:
-            pt_model = f'{model_name}-{shared.args.wbits}bit.pt'
+            pt_model = f'{model_name}-{shared.args.gptq_bits}bit'
 
-        # Try to find the .pt both in models/ and in the subfolder
-        for path in [Path(p) for p in [f"models/{pt_model}", f"{path_to_model}/{pt_model}"]]:
+        # Try to find the .safetensors or .pt both in models/ and in the subfolder
+        pt_path = None
+        for path in [Path(p+ext) for ext in ['.safetensors', '.pt'] for p in [f"models/{pt_model}", f"{path_to_model}/{pt_model}"]]:
             if path.exists():
+                print(f"Found {path}")
                 pt_path = path
+                break
 
     if not pt_path:
         print(f"Could not find {pt_model}, exiting...")
