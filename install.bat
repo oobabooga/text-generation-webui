85e4ec6e6becfb229c50688de242346d5f30c402
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-03-30 18:22:48 -0300

    Download the cuda branch directly

--- install.bat
+++ install.bat
@@ -91,9 +91,8 @@ if not exist repositories\ (
 )
 cd repositories || goto end
 if not exist GPTQ-for-LLaMa\ (
-  git clone https://github.com/qwopqwop200/GPTQ-for-LLaMa.git
+  git clone https://github.com/qwopqwop200/GPTQ-for-LLaMa.git -b cuda
   cd GPTQ-for-LLaMa || goto end
-  git checkout cuda
   call python -m pip install -r requirements.txt
   call python setup_cuda.py install
   if not exist "%INSTALL_ENV_DIR%\lib\site-packages\quant_cuda-0.0.0-py3.10-win-amd64.egg" (
