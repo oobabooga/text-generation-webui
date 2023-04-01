1399c012541ac779b1a55c88d516c1ef76fdaff4
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-04-02 02:40:27 +0300

    changes in syste controller

--- extensions/pyrogram_bot/controllers/system.py
+++ extensions/pyrogram_bot/controllers/system.py
@@ -1,15 +1,26 @@
+import json
+
 from pyrogram.types import Message
 
 from modules import shared
 
+pre = lambda text: f"```\n{text}\n```"
+
 async def gradio(_, msg: Message) -> None:
   print(shared.gradio, flush=True)
-  await msg.reply(shared.gradio)
+  await msg.reply(pre(str(shared.gradio)[-4000:]))
   msg.stop_propagation()
 
 async def settings(_, msg: Message) -> None:
-  print(shared.settings, flush=True)
-  await msg.reply(shared.settings)
+  text = json.dumps(shared.settings, indent=2)
+  print(text, flush=True)
+  await msg.reply(pre(text))
+  msg.stop_propagation()
+
+async def history(_, msg: Message) -> None:
+  text = json.dumps(shared.history['visible'], indent=2)
+  print(text, flush=True)
+  await msg.reply(pre(text))
   msg.stop_propagation()
 
 async def info(app, msg: Message) -> None:
@@ -24,5 +35,5 @@ async def info(app, msg: Message) -> None:
   for key, value in answer_dict.items():
     answer += f"{key}: {value}\n"
 
-  await msg.reply(answer)
+  await msg.reply(pre(answer))
   msg.stop_propagation()
