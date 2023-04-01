71e4c3ac8a444f08dc28bfc3d321b3b34f205c86
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-04-02 02:41:52 +0300

    made client init safer

--- extensions/pyrogram_bot/client.py
+++ extensions/pyrogram_bot/client.py
@@ -5,24 +5,33 @@ from os.path import dirname, relpath, join
 from pyrogram import Client as PyroClient, filters
 from pyrogram.handlers import MessageHandler
 
-from .controllers import ai, get_chars, get_models, set_char, set_model, info, gradio, settings
+from .controllers import (
+    ai,
+    get_chars, get_models,
+    set_char, set_model,
+    info, history, gradio, settings
+)
 from .services.i18n import get_i18n
-
-allowed_chat = int(env["TELEGRAM_CHAT_ID"])
-bot_owner = int(env["TELEGRAM_BOT_OWNER_ID"])
 
 class Client(PyroClient):
   lang = "en"
   t = get_i18n(lang)
   cwd = dirname(relpath(__file__, getcwd()))
+  allowed_chat = 0
+  bot_owner = 0
 
   def __init__(self):
-    super().__init__(
-      join(self.cwd, 'textgen'),
-      api_id    = int(env["TELEGRAM_API_ID"]),
-      api_hash  = str(env["TELEGRAM_API_HASH"]),
-      bot_token = str(env["TELEGRAM_BOT_TOKEN"]),
-    )
+    try:
+      super().__init__(
+        join(self.cwd, 'textgen'),
+        api_id    = int(env["TELEGRAM_API_ID"]),
+        api_hash  = str(env["TELEGRAM_API_HASH"]),
+        bot_token = str(env["TELEGRAM_BOT_TOKEN"]),
+      )
+      self.allowed_chat = int(env["TELEGRAM_CHAT_ID"])
+      self.bot_owner = int(env["TELEGRAM_BOT_OWNER_ID"])
+    except KeyError:
+      raise EnvironmentError(self.t('error.credentials_not_found'))
 
   async def run(self):
     try:
@@ -40,7 +49,8 @@ class Client(PyroClient):
     self.add_handler(MessageHandler(set_char,  filters.command(["set_character"])))
     self.add_handler(MessageHandler(get_models,filters.command(["models"])))
     self.add_handler(MessageHandler(set_model, filters.command(["set_model"])))
+    self.add_handler(MessageHandler(info,      filters.command(["info"])))
+    self.add_handler(MessageHandler(history,   filters.command(["history"])))
     self.add_handler(MessageHandler(gradio,    filters.command(["gradio"])))
     self.add_handler(MessageHandler(settings,  filters.command(["settings"])))
-    self.add_handler(MessageHandler(info,      filters.command(["info"])))
-    self.add_handler(MessageHandler(ai,        filters.chat(allowed_chat)), 100)
+    self.add_handler(MessageHandler(ai,        filters.chat(self.allowed_chat)), 100)
