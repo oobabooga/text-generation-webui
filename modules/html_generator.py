8203ce0cac79d84b4ecafc8378c83cdf5b21b68f
Forkoz <59298527+Ph0rk0z@users.noreply.github.com>
2023-04-05 12:25:01 -0500

    Stop character pic from being cached when changing chars or clearing. (#798)
    Tested on both FF and chromium

--- modules/html_generator.py
+++ modules/html_generator.py
@@ -155,7 +155,7 @@ def generate_cai_chat_html(history, name1, name2, reset_cache=False):
     output = f'<style>{cai_css}</style><div class="chat" id="chat">'
 
     # The time.time() is to prevent the brower from caching the image
-    suffix = f"?{time.time()}" if reset_cache else ''
+    suffix = f"?{time.time()}" if reset_cache else f"?{name2}"
     img_bot = f'<img src="file/cache/pfp_character.png{suffix}">' if Path("cache/pfp_character.png").exists() else ''
     img_me = f'<img src="file/cache/pfp_me.png{suffix}">' if Path("cache/pfp_me.png").exists() else ''
 
