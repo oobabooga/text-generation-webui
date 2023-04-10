254609daca1bc8e110f01e3d81f1c9b5ac60c00f
jllllll <3887729+jllllll@users.noreply.github.com>
2023-04-10 08:48:56 -0500

    Update llama-cpp-python link to official wheel (#19)

--- install.bat
+++ install.bat
@@ -83,7 +83,6 @@ if exist text-generation-webui\ (
 ) else (
   git clone https://github.com/oobabooga/text-generation-webui.git
   call python -m pip install https://github.com/jllllll/bitsandbytes-windows-webui/raw/main/bitsandbytes-0.37.2-py3-none-any.whl
-  call python -m pip install https://github.com/Loufe/llama-cpp-python/raw/main/wheels/llama_cpp_python-0.1.26-cp310-cp310-win_amd64.whl --no-deps  
   cd text-generation-webui || goto end
 )
 call python -m pip install -r requirements.txt --upgrade
--- start-webui.bat
+++ start-webui.bat
@@ -13,7 +13,7 @@ if not exist "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" (
 call "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" activate "%INSTALL_ENV_DIR%" || ( echo MicroMamba hook not found. && goto end )
 cd text-generation-webui
 
-call python server.py --auto-devices --cai-chat
+call python server.py --auto-devices --chat
 
 :end
 pause
