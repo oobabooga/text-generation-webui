c13e8651ad41b1043e22c3150255692e3bc3eb3f
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-04-13 12:09:42 -0300

    Suppress "TypedStorage is deprecated" warnings

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import os
+import warnings
 
 os.environ['GRADIO_ANALYTICS_ENABLED'] = 'False'
 os.environ['BITSANDBYTES_NOWELCOME'] = '1'
+warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
 
 import importlib
 import io
