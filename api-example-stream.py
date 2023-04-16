9a177b43d9c0b75568520a0e7307c536b9f596bb
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-04-16 14:22:07 -0300

    Fix a bug

--- api-example-stream.py
+++ api-example-stream.py
@@ -42,7 +42,7 @@ async def run(context):
         'early_stopping': False,
         'seed': -1,
         'add_bos_token': True,
-        'custom_stopping_strings': []
+        'custom_stopping_strings': [],
         'truncation_length': 2048,
         'ban_eos_token': False,
         'skip_special_tokens': True,
