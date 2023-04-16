7e598dc64fb9cde6e4d4b3ce834e3ff7790b5dd4
GuizzyQC <86683381+GuizzyQC@users.noreply.github.com>
2023-04-16 00:38:15 -0400

    Many improvements to
    Changed parsing library to trafilatura, giving much better results. Re-enabled scraping of results, with ability to select amount of results scraped. Also added toggle to disable adding result summaries.

--- extensions/searx_integration/script.py
+++ extensions/searx_integration/script.py
@@ -1,5 +1,5 @@
 import urllib.request
-import html2text
+import trafilatura
 import json
 import requests
 import io
@@ -15,11 +15,14 @@ torch._C._jit_set_profiling_mode(False)
 
 params = { # These can all be set in the settings.yml file
     'enable_search': False,
-    'include_first_result': False,
-    'max_bytes_first_result': 5000,
+    'include_first_results': True,
+    'include_result_summary': False,
+    'max_bytes_for_fetched_pages': 5000,
     'searx_instance': "",
     'max_bytes_total': 50000,
+    'extra_query_information': "!ddg",
     'removal_list': ['\t', '\n', '\\n', '\\t'],
+    'number_of_results': 1,
     'console_display': False
 }
 
@@ -38,33 +41,41 @@ def get_search_term(string): # This checks if you say "search ... about" somethi
         subject = string.split('about',1)[1]
         return subject
 
+
 def search_string(search_term): # This is the main logic that sends the API request to Searx and returns the text to add to the context
 
     global do_search
     print("Searching about" + search_term + "...")
     do_search = False
-    r = requests.get(params['searx_instance'], params={'q': search_term,'format': 'json','pageno': '1'})
+    query = search_term + " " + params['extra_query_information']
+    r = requests.get(params['searx_instance'], params={'q': query,'format': 'json','pageno': '1'})
     searchdata = r.json()
     searchdata = searchdata['results']
-    new_context = initial_context + search_term + ":"
-    if params['include_first_result']:
-        weburl = urllib.request.urlopen(searchdata[0]['url'])
-        page_content = html2text.html2text(str(weburl.read()))
-        page_content = re.sub(r"[\n\t]*", "",page_content)
-        for s in params['removal_list']:
-            page_content = page_content.replace(s, '')
-        page_content = io.StringIO(page_content)
-        new_context = new_context + page_content.read(params['max_bytes_first_result'])
-    for result in searchdata:
-        if 'content' in result:
-            summary = result['content']
-            new_context = new_context + "\n" + summary
+    new_context = initial_context + search_term + ":\n"
+    if params['include_first_results']:
+        i = 0
+        while i < params['number_of_results']:
+            webpage = trafilatura.fetch_url(searchdata[i]['url'])
+            page_content = trafilatura.extract(webpage, include_comments=False, include_tables=False, no_fallback=True)
+    #        page_content = html2text.html2text(str(weburl.read()))
+    #        page_content = re.sub(r"[\n\t]*", "",page_content)
+    #        for s in params['removal_list']:
+    #            page_content = page_content.replace(s, '')
+            page_content = io.StringIO(page_content)
+            new_context = new_context + page_content.read(params['max_bytes_for_fetched_pages'])
+            i = i + 1
+    if params['include_result_summary']:
+        for result in searchdata:
+            if 'content' in result:
+                summary = result['content']
+                new_context = new_context + "\n" + summary
     new_context = io.StringIO(new_context)
     new_context = new_context.read(params['max_bytes_total'])
-    new_context = new_context + "\n###"
+    new_context = new_context + "\n"
     if params['console_display']:
         print(new_context)
     return new_context
+
 
 def input_modifier(string):
 
@@ -72,7 +83,7 @@ def input_modifier(string):
         search_term = get_search_term(string)
         if do_search:
             search_result = search_string(search_term)
-            string = search_result + "\nNow here is my request: " + string
+            string = search_result + "\n\nUsing the information I just gave you, and not adding any thing new, respond to this request:" + string
     return string
 
 
@@ -83,15 +94,21 @@ def ui():
             with gr.Column():
                 enable_search = gr.Checkbox(value=params['enable_search'], label='Activate Searx integration')
                 console_display = gr.Checkbox(value=params['console_display'], label='Display search results on console')
-#                include_first_result = gr.Checkbox(value=params['include_first_result'], label='Insert text of first search result')
-#                max_bytes_first_result = gr.Slider(10000,100000,value=params['max_bytes_first_result'],step=100,label='Maximum bytes of first result added to context')
+                include_first_results = gr.Checkbox(value=params['include_first_results'], label='Insert text of search result')
+                number_of_results = gr.Slider(1,10,value=params['number_of_results'],step=1,label='Number of results to fetch')
             with gr.Column():
                 searx_instance = gr.Textbox(placeholder=params['searx_instance'], value=params['searx_instance'], label='Searx instance address')
+                extra_query_information = gr.Textbox(placeholder=params['extra_query_information'], value=params['extra_query_information'], label='Extra info to pass in Searx query')
+                include_result_summary = gr.Checkbox(value=params['include_result_summary'], label='Insert summary of first page of search results')
+                max_bytes_for_fetched_pages = gr.Slider(10000,100000,value=params['max_bytes_for_fetched_pages'],step=100,label='Maximum bytes of fetched pages added to context')
                 max_bytes_total = gr.Slider(10000,100000,value=params['max_bytes_total'],step=100,label='Total max bytes added to context')
 
     enable_search.change(lambda x: params.update({"enable_search": x}), enable_search, None)
     console_display.change(lambda x: params.update({"console_display": x}), console_display, None)
-#    include_first_result.change(lambda x: params.update({"include_first_result": x}), include_first_result, None)
-#    max_bytes_first_result.change(lambda x: params.update({"max_bytes_first_result": x}), max_bytes_first_result, None)
+    include_first_results.change(lambda x: params.update({"include_first_results": x}), include_first_results, None)
+    include_result_summary.change(lambda x: params.update({"include_result_summary": x}), include_result_summary, None)
+    number_of_results.change(lambda x: params.update({"number_of_results": x}), number_of_results, None)
+    max_bytes_for_fetched_pages.change(lambda x: params.update({"max_bytes_for_fetched_pages": x}), max_bytes_for_fetched_pages, None)
     searx_instance.change(lambda x: params.update({"searx_instance": x}), searx_instance, None)
+    extra_query_information.change(lambda x: params.update({"extra_query_information": x}), extra_query_information, None)
     max_bytes_total.change(lambda x: params.update({"max_bytes_total": x}), max_bytes_total, None)
