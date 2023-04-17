163ea295e78e2a7c4fcdf065b80893feb99e0748
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-04-17 13:54:15 -0300

    Fix but in API extension

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -78,7 +78,7 @@ class Handler(BaseHTTPRequestHandler):
 
             response = json.dumps({
                 'results': [{
-                    'text': answer[len(prompt):]
+                    'text': answer
                 }]
             })
             self.wfile.write(response.encode('utf-8'))
