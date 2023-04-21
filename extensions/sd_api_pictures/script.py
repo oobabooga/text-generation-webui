1ace0875837be852adbc627365f8f933e8f4aa6c
ImpactFrames <impactframes@outlook.com>
2023-04-21 02:27:01 +0100

    Update Script.py
    Batch count, Batch size, clip skip, interactive mode SD prompt of command, saves .txt with generation parameter, save stealth info on .png and respects interactive address. stealth info needs  https://github.com/ashen-sensored/sd_webui_stealth_pnginfo installed on A1111 webui to work

--- extensions/sd_api_pictures/script.py
+++ extensions/sd_api_pictures/script.py
@@ -7,10 +7,12 @@ from pathlib import Path
 from PIL import Image
 import torch
 import gradio as gr
+from PIL import Image, PngImagePlugin
 import modules.shared as shared
 import modules.chat as chat
 import requests
 from modules.models import reload_model, unload_model
+import json
 
 
 
@@ -27,21 +29,22 @@ params = {
     'prompt_prefix': "(Masterpiece:1.1), detailed, intricate, colorful",
     'negative_prompt': "(worst quality, low quality:1.3),NSFW,Nudity,(Child:2),extra_fingers,deform,monochrome",
     'enable_hr': False,
-    'denoising_strength': 0.36,
+    'denoising_strength': 0.3,
     'hr_scale': 2,
     'hr_upscaler': "4x_NMKD-Superscale-SP_178000_G",
     'hr_second_pass_steps': 0,
     'seed': -1,
-    'batch_size': 1,
+    'subseed': 0,
+    'batch_size': 4,
     'n_iter': 1,
-    'steps': 36,
+    'steps': 24,
     'cfg_scale': 7,
     'width': 512,
     'height': 768,
     'restore_faces': False,
     'sampler_name': "DPM++ SDE Karras",
-    'subseed': -1,
     'n_iter': 1,
+    'CLIP_stop_at_last_layers': 2,
     'upscalers' : [],
     'samplers' : [],
     'sd_models' : []
@@ -102,28 +105,36 @@ def triggers_are_in(string):
     # regex searches for send|main|message|me (at the end of the word) followed by
     # a whole word of image|pic|picture|photo|snap|snapshot|selfie|meme(s),
     # (?aims) are regex parser flags
-    return bool(re.search('(?aims)(send|mail|message|me)\\b.+?\\b(image|pic(ture)?|photo|snap(shot)?|selfie|meme)s?\\b', string))
-
+    trigger_found = bool(re.search('(?aims)(send|mail|message|me)\\b.+?\\b(image|pic(ture)?|photo|snap(shot)?|selfie|meme)s?\\b', string))
+    sd_prompt_match = re.search('(?aims)\\b(sd)\\b.+?\\b(prompt)\\b', string)
+    sd_prompt_found = bool(sd_prompt_match)
+    return trigger_found, sd_prompt_found, sd_prompt_match
 
 def input_modifier(string):
-    """
-    This function is applied to your text inputs before
-    they are fed into the model.
-    """
-
     global params
 
-    if not params['mode'] == 1:  # if not in immersive/interactive mode, do nothing
+    if not params['mode'] == 1:
         return string
 
-    if triggers_are_in(string):  # if we're in it, check for trigger words
+    trigger_found, sd_prompt_found, sd_prompt_match = triggers_are_in(string)
+    if trigger_found:
         toggle_generation(True)
         string = string.lower()
         if "of" in string:
-            subject = string.split('of', 1)[1]  # subdivide the string once by the first 'of' instance and get what's coming after it
+            subject = string.split('of', 1)[1]
             string = "Please provide a detailed and vivid description of " + subject
         else:
             string = "Please provide a detailed description of your appearance, your surroundings and what you are doing right now"
+    elif sd_prompt_found:
+        start_pos = sd_prompt_match.end()
+        instruction = string[start_pos:].strip()
+
+        if "of" in instruction:
+            split_string = instruction.split('of', 1)
+            instruction = split_string[1]
+            string = "Provide a coherent Stable Diffusion prompt that matches the subject of" + instruction
+        else:
+            string = "improve the prompt following the format components scheme style of SD " + instruction
 
     return string
 
@@ -135,8 +146,10 @@ def apply_settings():
     global params
 
     payload_options = {
-        "sd_model_checkpoint": params['sd_model_checkpoint']
-        #"sd_vae": params['sd_vae'] # will try later
+        "sd_model_checkpoint": params['sd_model_checkpoint'],
+        "CLIP_stop_at_last_layers": params['CLIP_stop_at_last_layers']
+
+        #"sd_vae": params['sd_vae'] #can't find a way to retieve them, will try later
     }
 
 
@@ -169,12 +182,11 @@ def get_SD_pictures(description):
         "hr_upscaler": params['hr_upscaler'],
         "hr_scale": params['hr_scale'],
         "restore_faces": params['restore_faces'],
-        "batch_size": 1,
+        "batch_size": params['batch_size'],
         "denoising_strength": params['denoising_strength'],
         "hr_second_pass_steps": params['hr_second_pass_steps'],
-        "subseed": -1,
-        "n_iter": 1
-
+        "n_iter": params['n_iter'],
+        'subseed': 0
     }
 
     print(f'Prompting the image generator via the API on {params["address"]}...')
@@ -183,18 +195,64 @@ def get_SD_pictures(description):
     r = response.json()
 
     visible_result = ""
+    image_count = 0
     for img_str in r['images']:
+        image_count += 1
         if params['save_img']:
             img_data = base64.b64decode(img_str)
 
             variadic = f'{date.today().strftime("%Y_%m_%d")}/{shared.character}_{int(time.time())}'
-            output_file = Path(f'extensions/sd_api_pictures/outputs/{variadic}.png')
+
+            # grab png info
+            png_payload = {
+                "image": "data:image/png;base64," + img_str
+            }
+            png_response = requests.post(url=f'{params["address"]}/sdapi/v1/png-info', json=png_payload)
+            png_info = png_response.json()
+
+            if png_response.status_code == 200:
+                str_parameters = png_info.get("info", "")
+            else:
+                str_parameters = ""
+            # Save image with metadata
+            image = Image.open(io.BytesIO(base64.b64decode(img_str)))
+            # Add stealth pnginfo for this stuff you have to download the script to stealth png info https://github.com/ashen-sensored/sd_webui_stealth_pnginfo then all the generated images will have the metadata that you can rig by dragging into the png info tab in A1111
+            image.putalpha(255)
+            pixels = image.load()
+            str_parameters = png_response.json().get("info")
+            signature_str = 'stealth_pnginfo'
+            binary_signature = ''.join(format(byte, '08b') for byte in signature_str.encode('utf-8'))
+            binary_param = ''.join(format(byte, '08b') for byte in str_parameters.encode('utf-8'))
+            param_len = len(binary_param)
+            binary_param_len = format(param_len, '032b')
+            binary_data = binary_signature + binary_param_len + binary_param
+            index = 0
+            for x in range(image.width):
+                for y in range(image.height):
+                    if index < len(binary_data):
+                        r, g, b, a = pixels[x, y]
+                        a = (a & ~1) | int(binary_data[index])
+                        pixels[x, y] = (r, g, b, a)
+                        index += 1
+                    else:
+                        break
+
+            output_file = Path(f'extensions/sd_api_pictures/outputs/{variadic}_{image_count}.png')
             output_file.parent.mkdir(parents=True, exist_ok=True)
 
-            with open(output_file.as_posix(), 'wb') as f:
-                f.write(img_data)
+            metadata = PngImagePlugin.PngInfo()
+            metadata.add_text("parameters", str_parameters)
 
-            visible_result = visible_result + f'<img src="/file/extensions/sd_api_pictures/outputs/{variadic}.png" alt="{description}" style="max-width: unset; max-height: unset;">\n'
+            
+
+            # Save image with metadata and stealth pnginfo
+            image.save(output_file.as_posix(), 'PNG', pnginfo=metadata)
+            # Save text file with metadata
+            info_output_file = Path(f'extensions/sd_api_pictures/outputs/{variadic}_{image_count}.txt')
+            with open(info_output_file, 'w') as info_file:
+                info_file.write(str(str_parameters))
+
+            visible_result = visible_result + f'<img src="/file/extensions/sd_api_pictures/outputs/{variadic}_{image_count}.png" alt="{description}" style="max-width: unset; max-height: unset;">\n'
         else:
             image = Image.open(io.BytesIO(base64.b64decode(img_str.split(",", 1)[0])))
             # lower the resolution of received images for the chat, otherwise the log size gets out of control quickly with all the base64 values in visible history
@@ -205,6 +263,8 @@ def get_SD_pictures(description):
             image_bytes = buffered.getvalue()
             img_str = "data:image/jpeg;base64," + base64.b64encode(image_bytes).decode()
             visible_result = visible_result + f'<img src="{img_str}" alt="{description}">\n'
+
+            pass
 
     if params['manage_VRAM']:
         give_VRAM_priority('LLM')
@@ -345,9 +405,11 @@ def ui():
             with gr.Column():
                 steps = gr.Number(label="Steps:", value=params['steps'])
                 seed = gr.Number(label="Seed:", value=params['seed'])
+                CLIP_stop_at_last_layers= gr.Slider(1, 4, value=params['CLIP_stop_at_last_layers'], step=1, label='clipskip')
                 with gr.Column(scale=1, min_width=300):
                     cfg_scale = gr.Number(label="CFG Scale:", value=params['cfg_scale'])
-                    #batch_size = gr.Slider(1, 6, value=params['batch_size'], step=1, label='batch size')
+                    n_iter = gr.Number(label="Batch count:", value=params['n_iter'])
+                    batch_size = gr.Slider(1, 6, value=params['batch_size'], step=1, label='batch size')
 
     
     address.change(lambda x: params.update({"address": filter_address(x)}), address, None)
@@ -367,21 +429,18 @@ def ui():
     steps.change(lambda x: params.update({"steps": x}), steps, None)
     seed.change(lambda x: params.update({"seed": x}), seed, None)
     cfg_scale.change(lambda x: params.update({"cfg_scale": x}), cfg_scale, None)
-
+    n_iter.change(lambda x: params.update({"n_iter": x}), n_iter, None)
     hr_upscaler.change(lambda x: params.update({"hr_upscaler": x}), hr_upscaler, None)
     sampler_name.change(lambda x: params.update({"sampler_name": x}), sampler_name, None)
     sd_model_checkpoint.change(lambda x: params.update({"sd_model_checkpoint": x}), sd_model_checkpoint, None)
-    #batch_size.change(lambda x: params.update({"batch_size": x}), batch_size, None)
+    batch_size.change(lambda x: params.update({"batch_size": x}), batch_size, None)
     restore_faces.change(lambda x: params.update({"restore_faces": x}), restore_faces, None)
     enable_hr.change(lambda x: params.update({"enable_hr": x}), enable_hr, None)
     denoising_strength.change(lambda x: params.update({"denoising_strength": x}), denoising_strength, None)
     hr_second_pass_steps.change(lambda x: params.update({"hr_second_pass_steps": x}), hr_second_pass_steps, None)
     hr_scale.change(lambda x: params.update({"hr_scale": x}), hr_scale, None)
-
+    CLIP_stop_at_last_layers.change(lambda x: params.update({"CLIP_stop_at_last_layers": x}), CLIP_stop_at_last_layers, None)
     force_pic.click(lambda x: toggle_generation(True), inputs=force_pic, outputs=None)
     suppr_pic.click(lambda x: toggle_generation(False), inputs=suppr_pic, outputs=None)
 
     Apply.click(lambda x: apply_settings(), inputs=Apply, outputs=None)
-
-
-
