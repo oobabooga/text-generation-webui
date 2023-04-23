7d80d9d8ef2cc4213554d8987eb043a10fd61e15
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-04-23 14:43:29 -0300

    Change a comment

--- api-example.py
+++ api-example.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-# For local streaming, the websockets are hosted without ssl - ws://
+# For local streaming, the websockets are hosted without ssl - http://
 HOST = 'localhost:5000'
 URI = f'http://{HOST}/api/v1/generate'
 
