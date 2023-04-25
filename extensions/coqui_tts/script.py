41cdfcc82803aa8d03fcc3666513c5c80bfd62e6
da3dsoul <da3dsoul@gmail.com>
2023-04-25 16:47:58 -0400

    Coqui UI Improvements

--- extensions/coqui_tts/script.py
+++ extensions/coqui_tts/script.py
@@ -1,5 +1,9 @@
 import time
+import traceback
 from pathlib import Path
+
+import TTS.utils.synthesizer
+
 from modules import chat, shared, tts_preprocessor
 from modules.html_generator import chat_html_wrapper
 
@@ -23,37 +27,43 @@ current_params = params.copy()
 speakers = []
 languages = []
 models = [
-    'tts_models/en/ek1/tacotron2',
-    'tts_models/en/ljspeech/tacotron2-DDC',
-    'tts_models/en/ljspeech/tacotron2-DDC_ph',
-    'tts_models/en/ljspeech/glow-tts',
-    'tts_models/en/ljspeech/speedy-speech',
-    'tts_models/en/ljspeech/tacotron2-DCA',
-    'tts_models/en/ljspeech/vits',
-    'tts_models/en/ljspeech/vits--neon',
-    'tts_models/en/ljspeech/fast_pitch',
-    'tts_models/en/ljspeech/overflow',
-    'tts_models/en/ljspeech/neural_hmm',
-    'tts_models/en/vctk/vits',
-    'tts_models/en/vctk/fast_pitch',
-    'tts_models/en/sam/tacotron-DDC',
-    'tts_models/en/blizzard2013/capacitron-t2-c50',
-    'tts_models/en/blizzard2013/capacitron-t2-c150_v2'
+    {'name': 'multi/your_tts', 'value': 'tts_models/multilingual/multi-dataset/your_tts'},
+    {'name': 'ek1/tacotron2', 'value': 'tts_models/en/ek1/tacotron2'},
+    {'name': 'ljspeech/tacotron2-DDC', 'value': 'tts_models/en/ljspeech/tacotron2-DDC'},
+    {'name': 'ljspeech/tacotron2-DDC_ph', 'value': 'tts_models/en/ljspeech/tacotron2-DDC_ph'},
+    {'name': 'ljspeech/glow-tts', 'value': 'tts_models/en/ljspeech/glow-tts'},
+    {'name': 'ljspeech/speedy-speech', 'value': 'tts_models/en/ljspeech/speedy-speech'},
+    {'name': 'ljspeech/tacotron2-DCA', 'value': 'tts_models/en/ljspeech/tacotron2-DCA'},
+    {'name': 'ljspeech/vits (espeak)', 'value': 'tts_models/en/ljspeech/vits'},
+    {'name': 'ljspeech/vits--neon (espeak)', 'value': 'tts_models/en/ljspeech/vits--neon'},
+    {'name': 'ljspeech/fast_pitch', 'value': 'tts_models/en/ljspeech/fast_pitch'},
+    {'name': 'ljspeech/overflow (espeak)', 'value': 'tts_models/en/ljspeech/overflow'},
+    {'name': 'ljspeech/neural_hmm (espeak)', 'value': 'tts_models/en/ljspeech/neural_hmm'},
+    {'name': 'vctk/vits (espeak)', 'value': 'tts_models/en/vctk/vits'},
+    {'name': 'vctk/fast_pitch', 'value': 'tts_models/en/vctk/fast_pitch'},
+    {'name': 'sam/tacotron-DDC (espeak)', 'value': 'tts_models/en/sam/tacotron-DDC'},
+    {'name': 'blizzard2013/capacitron-t2-c50 (espeak)', 'value': 'tts_models/en/blizzard2013/capacitron-t2-c50'},
+    {'name': 'blizzard2013/capacitron-t2-c150_v2 (espeak)', 'value': 'tts_models/en/blizzard2013/capacitron-t2-c150_v2'}
 ]
+
+model_values = [i['value'] for i in models]
+model_choices = [i['name'] for i in models]
 
 
 def load_model():
     # Init TTS
     global speakers, languages
     tts = TTS(params['model_id'], gpu=params['Cuda'])
-    if tts is not None and tts.synthesizer is not None and tts.synthesizer.tts_config is not None and hasattr(tts.synthesizer.tts_config, 'num_chars'):
+    if tts is not None and tts.synthesizer is not None and tts.synthesizer.tts_config is not None and hasattr(
+            tts.synthesizer.tts_config, 'num_chars'):
         tts.synthesizer.tts_config.num_chars = 250
 
     speakers = tts.speakers if tts.speakers is not None else []
     temp_speaker = params['speaker'] if params['speaker'] in speakers else speakers[0] if len(speakers) > 0 else None
 
     languages = tts.languages if tts.languages is not None else []
-    temp_language = params['language'] if params['language'] in languages else languages[0] if len(languages) > 0 else None
+    temp_language = params['language'] if params['language'] in languages else languages[0] if len(
+        languages) > 0 else None
 
     return tts, temp_speaker, temp_language
 
@@ -74,9 +84,11 @@ def toggle_text_in_history(name1, name2, mode):
         if visible_reply.startswith('<audio'):
             if params['show_text']:
                 reply = shared.history['internal'][i][1]
-                shared.history['visible'][i] = [shared.history['visible'][i][0], f"{visible_reply.split('</audio>')[0]}</audio>\n\n{reply}"]
+                shared.history['visible'][i] = [shared.history['visible'][i][0],
+                                                f"{visible_reply.split('</audio>')[0]}</audio>\n\n{reply}"]
             else:
-                shared.history['visible'][i] = [shared.history['visible'][i][0], f"{visible_reply.split('</audio>')[0]}</audio>"]
+                shared.history['visible'][i] = [shared.history['visible'][i][0],
+                                                f"{visible_reply.split('</audio>')[0]}</audio>"]
     return chat_html_wrapper(shared.history['visible'], name1, name2, mode)
 
 
@@ -88,7 +100,8 @@ def input_modifier(string):
 
     # Remove autoplay from the last reply
     if shared.is_chat() and len(shared.history['internal']) > 0:
-        shared.history['visible'][-1] = [shared.history['visible'][-1][0], shared.history['visible'][-1][1].replace('controls autoplay>', 'controls>')]
+        shared.history['visible'][-1] = [shared.history['visible'][-1][0],
+                                         shared.history['visible'][-1][1].replace('controls autoplay>', 'controls>')]
 
     shared.processing_message = "*Is recording a voice message...*"
     shared.args.no_stream = True  # Disable streaming cause otherwise the audio output will stutter and begin anew every time the message is being updated
@@ -160,7 +173,9 @@ def ui():
             autoplay = gr.Checkbox(value=params['autoplay'], label='Play TTS automatically')
 
         show_text = gr.Checkbox(value=params['show_text'], label='Show message text under audio player')
-        model_dropdown = gr.Dropdown(value=params['model_id'], choices=models, label='Model')
+        model_dropdown = gr.Dropdown(
+            value=model_choices[model_values.index(params['model_id'])] if params['model_id'] in model_values else None,
+            choices=model_choices, type='index', label='Model')
         speaker_dropdown = gr.Dropdown(value=params['speaker'],
                                        choices=model.speakers if model.speakers is not None else [], label='Speaker')
         language_dropdown = gr.Dropdown(value=params['language'],
@@ -175,15 +190,20 @@ def ui():
 
     # Convert history with confirmation
     convert_arr = [convert_confirm, convert, convert_cancel]
-    convert.click(lambda: [gr.update(visible=True), gr.update(visible=False), gr.update(visible=True)], None, convert_arr)
-    convert_confirm.click(lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None, convert_arr)
-    convert_confirm.click(remove_tts_from_history, [shared.gradio[k] for k in ['name1', 'name2', 'mode']], shared.gradio['display'])
+    convert.click(lambda: [gr.update(visible=True), gr.update(visible=False), gr.update(visible=True)], None,
+                  convert_arr)
+    convert_confirm.click(lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None,
+                          convert_arr)
+    convert_confirm.click(remove_tts_from_history, [shared.gradio[k] for k in ['name1', 'name2', 'mode']],
+                          shared.gradio['display'])
     convert_confirm.click(lambda: chat.save_history(mode='chat', timestamp=False), [], [], show_progress=False)
-    convert_cancel.click(lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None, convert_arr)
+    convert_cancel.click(lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None,
+                         convert_arr)
 
     # Toggle message text in history
     show_text.change(lambda x: params.update({"show_text": x}), show_text, None)
-    show_text.change(toggle_text_in_history, [shared.gradio[k] for k in ['name1', 'name2', 'mode']], shared.gradio['display'])
+    show_text.change(toggle_text_in_history, [shared.gradio[k] for k in ['name1', 'name2', 'mode']],
+                     shared.gradio['display'])
     show_text.change(lambda: chat.save_history(mode='chat', timestamp=False), [], [], show_progress=False)
 
     # Event functions to update the parameters in the backend
@@ -195,8 +215,12 @@ def ui():
     vc_textbox.change(lambda x: params.update({"voice_clone_reference_path": x}), vc_textbox, None)
 
 
-def update_model(x, speaker_dropdown, language_dropdown):
-    params.update({"model_id": x})
+def update_model(x):
+    model_id = model_values[x]
+    params.update({"model_id": model_id})
     global model, speaker, language, speakers, languages
-    model, speaker, language = load_model()
+    try:
+        model, speaker, language = load_model()
+    except:
+        print(traceback.format_exc())
     return [gr.update(value=speaker, choices=speakers), gr.update(value=language, choices=languages)]
