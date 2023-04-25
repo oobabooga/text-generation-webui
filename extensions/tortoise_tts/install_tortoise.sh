86b2097e46fb8153c26f29a2206e24bbec8fcade
da3dsoul <da3dsoul@gmail.com>
2023-04-25 13:31:30 -0400

    Make the tortoise installers use normal clones

--- extensions/tortoise_tts/install_tortoise.sh
+++ extensions/tortoise_tts/install_tortoise.sh
@@ -1,5 +1,4 @@
-git clone -n --depth=1 --filter=tree:0 https://github.com/neonbjb/tortoise-tts.git tortoise
+git clone https://github.com/neonbjb/tortoise-tts.git tortoise
 cd tortoise
-git sparse-checkout set --no-cone requirements.txt tortoise
 python -m pip install -r ./requirements.txt
 cd ../
