8aa9dcd3747d031bdf87d9e867e1c532d4eb180a
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-04-26 19:13:23 -0300

    Add an input field for the data

--- extensions/superbig/script.py
+++ extensions/superbig/script.py
@@ -1,7 +1,13 @@
+import gradio as gr
+
+from modules import shared
+
 from .provider import PseudocontextProvider, make_instruct_provider
 
 provider = PseudocontextProvider()
 instruct_provider = make_instruct_provider()
+
+data = ''
 
 def input_modifier(string):
     """
@@ -9,7 +15,14 @@ def input_modifier(string):
     they are fed into the model.
     """
 
+    # Use data here
+
     return instruct_provider.with_pseudocontext(string)
 
 def ui():
-    pass
+    if shared.is_chat():
+        # Chat mode has to be handled differently, probably using a custom_generate_chat_prompt
+        pass
+    else:
+        data_input = gr.Textbox(lines=20, label='Input data')
+        data_input.change(lambda x: globals().update(data=x))
