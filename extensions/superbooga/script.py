6b67cb6611e8b80e1202336637242e49c2575c51
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-05-07 15:01:14 -0300

    Generalize superbooga to chat mode

--- extensions/superbooga/script.py
+++ extensions/superbooga/script.py
@@ -8,8 +8,9 @@ import posthog
 import torch
 from bs4 import BeautifulSoup
 from chromadb.config import Settings
-from modules import shared
 from sentence_transformers import SentenceTransformer
+
+from modules import chat, shared
 
 print('Intercepting all calls to posthog :)')
 posthog.capture = lambda *args, **kwargs: None
@@ -53,6 +54,10 @@ class ChromaCollector(Collecter):
         result = self.collection.query(query_texts=search_strings, n_results=n_results, include=['documents'])['documents'][0]
         return result
 
+    def get_ids(self, search_strings: list[str], n_results: int) -> list[str]:
+        result = self.collection.query(query_texts=search_strings, n_results=n_results, include=['documents'])['ids'][0]
+        return list(map(lambda x : int(x[2:]), result))
+
     def clear(self):
         self.collection.delete(ids=self.ids)
 
@@ -68,18 +73,24 @@ collector = ChromaCollector(embedder)
 chunk_count = 5
 
 
-def feed_data_into_collector(corpus, chunk_len):
+def add_chunks_to_collector(chunks):
     global collector
-    chunk_len = int(chunk_len)
+    collector.clear()
+    collector.add(chunks)
 
+
+def feed_data_into_collector(corpus, chunk_len):
+    # Defining variables
+    chunk_len = int(chunk_len)
     cumulative = ''
+
+    # Breaking the data into chunks and adding those to the db
     cumulative += "Breaking the input dataset...\n\n"
     yield cumulative
     data_chunks = [corpus[i:i + chunk_len] for i in range(0, len(corpus), chunk_len)]
     cumulative += f"{len(data_chunks)} chunks have been found.\n\nAdding the chunks to the database...\n\n"
     yield cumulative
-    collector.clear()
-    collector.add(data_chunks)
+    add_chunks_to_collector(data_chunks)
     cumulative += "Done."
     yield cumulative
 
@@ -123,6 +134,8 @@ def apply_settings(_chunk_count):
 
 
 def input_modifier(string):
+    if shared.is_chat():
+        return string
 
     # Find the user input
     pattern = re.compile(r"<\|begin-user-input\|>(.*?)<\|end-user-input\|>", re.DOTALL)
@@ -143,6 +156,23 @@ def input_modifier(string):
     return string
 
 
+def custom_generate_chat_prompt(user_input, state, **kwargs):
+    if len(shared.history['internal']) > 2 and user_input != '':
+        chunks = []
+        for i in range(len(shared.history['internal'])-1):
+            chunks.append('\n'.join(shared.history['internal'][i]))
+
+        add_chunks_to_collector(chunks)
+        query = '\n'.join(shared.history['internal'][-1] + [user_input])
+        best_ids = collector.get_ids(query, n_results=len(shared.history['internal'])-1)
+
+        # Sort the history by relevance instead of by chronological order,
+        # except for the latest message
+        state['history'] = [shared.history['internal'][id_] for id_ in best_ids[::-1]] + [shared.history['internal'][-1]]
+
+    return chat.generate_chat_prompt(user_input, state, **kwargs)
+
+
 def ui():
     with gr.Accordion("Click for more information...", open=False):
         gr.Markdown(textwrap.dedent("""
@@ -156,7 +186,9 @@ def ui():
 
         It is a modified version of the superbig extension by kaiokendev: https://github.com/kaiokendev/superbig
 
-        ## How to use it
+        ## Notebook/default modes
+
+        ### How to use it
 
         1) Paste your input text (of whatever length) into the text box below.
         2) Click on "Load data" to feed this text into the Chroma database.
@@ -166,7 +198,7 @@ def ui():
 
         The special tokens mentioned above (`<|begin-user-input|>`, `<|end-user-input|>`, and `<|injection-point|>`) are removed when the injection happens.
 
-        ## Example
+        ### Example
 
         For your convenience, you can use the following prompt as a starting point (for Alpaca models):
 
@@ -186,14 +218,25 @@ def ui():
         ### Response:
         ```
 
+        ## Chat mode
+
+        In chat mode, the extension automatically sorts the history by relevance instead of chronologically, except for the very latest input/reply pair. 
+
+        That is, the prompt will include (starting from the end):
+
+        * Your input
+        * The latest input/reply pair
+        * The #1 most relevant input/reply pair prior to the latest
+        * The #2 most relevant input/reply pair prior to the latest
+        * Etc
+
+        This way, the bot can have a long term history.
+
         *This extension is currently experimental and under development.*
 
         """))
 
-    if shared.is_chat():
-        # Chat mode has to be handled differently, probably using a custom_generate_chat_prompt
-        pass
-    else:
+    if not shared.is_chat():
         with gr.Row():
             with gr.Column():
                 with gr.Tab("Text input"):
