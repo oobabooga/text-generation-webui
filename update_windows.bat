29727c6502701b960c2c299fa63f502b3ef3476c
jllllll <3887729+jllllll@users.noreply.github.com>
2023-05-08 23:49:27 -0500

    Fix Windows PATH fix (#57)

--- update_windows.bat
+++ update_windows.bat
@@ -2,9 +2,9 @@
 
 cd /D "%~dp0"
 
-echo "%CD%"| findstr /C:" " >nul && echo This script relies on Miniconda which can not be silently installed under a path with spaces. && goto end
-
 set PATH=%PATH%;%SystemRoot%\system32
+
+echo "%CD%"| findstr /C:" " >nul && echo This script relies on Miniconda which can not be silently installed under a path with spaces. && goto end
 
 @rem fix failed install when installing to a separate drive
 set TMP=%cd%\installer_files
