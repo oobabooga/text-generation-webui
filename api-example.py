11565a505361f7ff9e94faf6020a77edde54b600
Anchen <li.anchen.au@gmail.com>
2023-05-13 18:25:19 +1000

    fix the API example for missing stream key error.

--- api-example.py
+++ api-example.py
@@ -28,6 +28,7 @@ def run(prompt):
         'add_bos_token': True,
         'truncation_length': 2048,
         'ban_eos_token': False,
+        'stream': False,
         'skip_special_tokens': True,
         'stopping_strings': []
     }
