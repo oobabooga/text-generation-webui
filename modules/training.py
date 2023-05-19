5a007612cd3a6c9b4a51283706396dbff50fb092
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-05-19 12:00:36 -0300

    Minor changes

--- modules/training.py
+++ modules/training.py
@@ -36,8 +36,8 @@ except:
         "GPTNeoXForCausalLM": "gpt_neox"
     }
 
-WANT_INTERRUPT = False
 
+WANT_INTERRUPT = False
 PARAMETERS = ["lora_name", "always_override", "save_steps", "micro_batch_size", "batch_size", "epochs", "learning_rate", "lr_scheduler_type", "lora_rank", "lora_alpha", "lora_dropout", "cutoff_len", "dataset", "eval_dataset", "format", "eval_steps", "raw_text_file", "overlap_len", "newline_favor_len", "higher_rank_limit", "warmup_steps", "optimizer", "hard_cut_string"]
 
 
@@ -179,7 +179,7 @@ def change_rank_limit(use_higher_ranks: bool):
 
 
 def clean_path(base_path: str, path: str):
-    """"Strips unusual symbols and forcibly builds a path as relative to the intended directory."""
+    """Strips unusual symbols and forcibly builds a path as relative to the intended directory."""
     # TODO: Probably could do with a security audit to guarantee there's no ways this can be bypassed to target an unwanted path.
     # Or swap it to a strict whitelist of [a-zA-Z_0-9]
     path = path.replace('\\', '/').replace('..', '_')
@@ -264,7 +264,6 @@ def do_train(lora_name: str, always_override: bool, save_steps: int, micro_batch
                 continue
 
             tokens = shared.tokenizer.encode(text_part)
-
             step = cutoff_len - overlap_len
             if step <= 0:
                 yield f"Error: overlap_len ({overlap_len}) cannot be greater than or equal to cutoff_len ({cutoff_len})"
@@ -273,7 +272,7 @@ def do_train(lora_name: str, always_override: bool, save_steps: int, micro_batch
             tokens = list(split_chunks(tokens, step))
             for i in range(1, len(tokens)):
                 tokens[i] = tokens[i - 1][-overlap_len:] + tokens[i]
-            
+
             out_tokens.extend(tokens)
             del tokens
 
