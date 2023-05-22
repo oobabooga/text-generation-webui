07233a85c4eab9021e4c150f32087d357b0b9064
nabelekm <nabelekm@gmail.com>
2023-05-22 04:37:43 +0200

    fixed num_chunks

--- extensions/api/streaming_api.py
+++ extensions/api/streaming_api.py
@@ -11,30 +11,40 @@ from modules.text_generation import generate_reply
 from extensions.superbooga.script import feed_url_into_collector
 from extensions.superbooga.chromadb import make_collector
 
-PATHS = ['/api/v1/stream','/api/v1/chat-stream','/api/v1/superstream', '/api/v1/feed_urls']
+PATHS = [
+    "/api/v1/stream",
+    "/api/v1/chat-stream",
+    "/api/v1/superstream",
+    "/api/v1/feed_urls",
+]
 
 collector = make_collector()
 chunk_count = 5
 
-def custom_generate_chat_prompt(user_input,chunk_count, **kwargs):
+
+def custom_generate_chat_prompt(user_input, chunk_count, **kwargs):
     global collector
     results = collector.get_sorted(user_input, n_results=chunk_count)
-    user_input = '### Memory:\n' + '\n'.join(results) +'\n' + user_input
+    user_input = "### Memory:\n" + "\n".join(results) + "\n" + user_input
     return user_input
 
-async def _handle_connection(websocket, path):
 
+async def _handle_connection(websocket, path):
     if path == PATHS[0]:
         async for message in websocket:
             message = json.loads(message)
 
-            prompt = message['prompt']
+            prompt = message["prompt"]
             generate_params = build_parameters(message)
-            stopping_strings = generate_params.pop('stopping_strings')
-            generate_params['stream'] = True
+            stopping_strings = generate_params.pop("stopping_strings")
+            generate_params["stream"] = True
 
             generator = generate_reply(
-                prompt, generate_params, stopping_strings=stopping_strings, is_chat=False)
+                prompt,
+                generate_params,
+                stopping_strings=stopping_strings,
+                is_chat=False,
+            )
 
             # As we stream, only send the new bytes.
             skip_index = 0
@@ -42,92 +52,121 @@ async def _handle_connection(websocket, path):
 
             for a in generator:
                 to_send = a[skip_index:]
-                await websocket.send(json.dumps({
-                    'event': 'text_stream',
-                    'message_num': message_num,
-                    'text': to_send
-                }))
+                await websocket.send(
+                    json.dumps(
+                        {
+                            "event": "text_stream",
+                            "message_num": message_num,
+                            "text": to_send,
+                        }
+                    )
+                )
 
                 await asyncio.sleep(0)
                 skip_index += len(to_send)
                 message_num += 1
 
-            await websocket.send(json.dumps({
-                'event': 'stream_end',
-                'message_num': message_num
-            }))
+            await websocket.send(
+                json.dumps({"event": "stream_end", "message_num": message_num})
+            )
 
     elif path == PATHS[1]:
         async for message in websocket:
             body = json.loads(message)
 
-            user_input = body['user_input']
-            history = body['history']
+            user_input = body["user_input"]
+            history = body["history"]
             generate_params = build_parameters(body, chat=True)
-            generate_params['stream'] = True
-            regenerate = body.get('regenerate', False)
-            _continue = body.get('_continue', False)
+            generate_params["stream"] = True
+            regenerate = body.get("regenerate", False)
+            _continue = body.get("_continue", False)
 
             generator = generate_chat_reply(
-                user_input, history, generate_params, regenerate=regenerate, _continue=_continue, loading_message=False)
+                user_input,
+                history,
+                generate_params,
+                regenerate=regenerate,
+                _continue=_continue,
+                loading_message=False,
+            )
 
             message_num = 0
             for a in generator:
-                await websocket.send(json.dumps({
-                    'event': 'text_stream',
-                    'message_num': message_num,
-                    'history': a
-                }))
+                await websocket.send(
+                    json.dumps(
+                        {
+                            "event": "text_stream",
+                            "message_num": message_num,
+                            "history": a,
+                        }
+                    )
+                )
 
                 await asyncio.sleep(0)
                 message_num += 1
 
-            await websocket.send(json.dumps({
-                'event': 'stream_end',
-                'message_num': message_num
-            }))
+            await websocket.send(
+                json.dumps({"event": "stream_end", "message_num": message_num})
+            )
     elif path == PATHS[2]:
         async for message in websocket:
             message = json.loads(message)
 
-            prompt = message['prompt']
+            prompt = message["prompt"]
             generate_params = build_parameters(message)
-            stopping_strings = generate_params.pop('stopping_strings')
-            generate_params['stream'] = True
+            stopping_strings = generate_params.pop("stopping_strings")
+            generate_params["stream"] = True
 
-            infused_prompt = custom_generate_chat_prompt(prompt,chunk_count= 10)
-            generator = generate_reply(infused_prompt, generate_params, stopping_strings=stopping_strings, is_chat=False)
-            
+            infused_prompt = custom_generate_chat_prompt(
+                prompt, chunk_count=chunk_count
+            )
+            generator = generate_reply(
+                infused_prompt,
+                generate_params,
+                stopping_strings=stopping_strings,
+                is_chat=False,
+            )
+
             # As we stream, only send the new bytes.
             skip_index = 0
             message_num = 0
 
             for a in generator:
                 to_send = a[skip_index:]
-                await websocket.send(json.dumps({
-                    'event': 'text_stream',
-                    'message_num': message_num,
-                    'text': to_send
-                }))
+                await websocket.send(
+                    json.dumps(
+                        {
+                            "event": "text_stream",
+                            "message_num": message_num,
+                            "text": to_send,
+                        }
+                    )
+                )
 
                 await asyncio.sleep(0)
 
                 skip_index += len(to_send)
                 message_num += 1
 
-            await websocket.send(json.dumps({
-                'event': 'stream_end',
-                'message_num': message_num
-            }))
+            await websocket.send(
+                json.dumps({"event": "stream_end", "message_num": message_num})
+            )
     elif path == PATHS[3]:
-         async for message in websocket:
+        async for message in websocket:
             urls = message
-            datafeed = feed_url_into_collector(urls=urls, chunk_len=400,  chunk_sep='', strong_cleanup='True', threads=4, collector=collector)
+            datafeed = feed_url_into_collector(
+                urls=urls,
+                chunk_len=400,
+                chunk_sep="",
+                strong_cleanup="True",
+                threads=4,
+                collector=collector,
+            )
             for output in datafeed:
-                print(output)          
+                print(output)
 
     elif path != PATHS:
-        print(f'Streaming api: unknown path: {path}')
+        print(f"Streaming api: unknown path: {path}")
         return
 
 
@@ -137,11 +176,11 @@ async def _run(host: str, port: int):
 
 
 def _run_server(port: int, share: bool = False):
-    address = '0.0.0.0' if shared.args.listen else '127.0.0.1'
+    address = "0.0.0.0" if shared.args.listen else "127.0.0.1"
 
     def on_start(public_url: str):
-        public_url = public_url.replace('https://', 'wss://')
-        print(f'Starting streaming server at public url {public_url}{PATHS}')
+        public_url = public_url.replace("https://", "wss://")
+        print(f"Starting streaming server at public url {public_url}{PATHS}")
 
     if share:
         try:
@@ -149,7 +188,7 @@ def _run_server(port: int, share: bool = False):
         except Exception as e:
             print(e)
     else:
-        print(f'Starting streaming server at ws://{address}:{port}{PATHS}')
+        print(f"Starting streaming server at ws://{address}:{port}{PATHS}")
 
     asyncio.run(_run(host=address, port=port))
 
