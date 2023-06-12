c81107cef53659bf33a1dd98c737d6139832c3cb
TheBloke <tomjobbins@gmail.com>
2023-06-12 13:57:30 +0100

    Small adjustment to CLI help text

--- modules/shared.py
+++ modules/shared.py
@@ -143,7 +143,7 @@ parser.add_argument('--gptq-for-llama', action='store_true', help='Use GPTQ-for-
 parser.add_argument('--autogptq', action='store_true', help='DEPRECATED')
 parser.add_argument('--triton', action='store_true', help='Use triton.')
 parser.add_argument('--no_inject_fused_attention', action='store_true', help='Do not use fused attention (lowers VRAM requirements).')
-parser.add_argument('--no_inject_fused_mlp', action='store_true', help='Do not use fused MLP with Triton models (lowers VRAM requirements).')
+parser.add_argument('--no_inject_fused_mlp', action='store_true', help='Triton mode only: Do not use fused MLP (lowers VRAM requirements).')
 parser.add_argument('--desc_act', action='store_true', help='For models that don\'t have a quantize_config.json, this parameter is used to define whether to set desc_act or not in BaseQuantizeConfig.')
 
 # FlexGen
