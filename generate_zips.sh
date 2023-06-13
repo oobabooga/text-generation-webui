c42f183d3f7c4518a0ce846eac7935613a6af44b
jllllll <3887729+jllllll@users.noreply.github.com>
2023-06-12 22:04:15 -0500

    Installer for WSL (#78)

--- generate_zips.sh
+++ generate_zips.sh
@@ -1,5 +1,6 @@
-mkdir oobabooga_{windows,linux,macos}
-for p in windows macos linux; do
-  cp {*$p*\.*,webui.py,INSTRUCTIONS.TXT} oobabooga_$p;
+mkdir oobabooga_{windows,linux,macos,wsl}
+for p in windows macos linux wsl; do
+  if [ "$p" == "wsl" ]; then cp {*$p*\.*,webui.py,INSTRUCTIONS-WSL.TXT} oobabooga_$p;
+  else cp {*$p*\.*,webui.py,INSTRUCTIONS.TXT} oobabooga_$p; fi
   zip -r oobabooga_$p.zip oobabooga_$p;
 done
