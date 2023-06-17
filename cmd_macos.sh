657049d7d01097c2e23508664d25a74374fc0aa1
jllllll <3887729+jllllll@users.noreply.github.com>
2023-06-17 17:09:42 -0500

    Fix cmd_macos.sh (#82)
    MacOS version of Bash does not support process substitution

--- cmd_macos.sh
+++ cmd_macos.sh
@@ -4,6 +4,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 if [[ "$(pwd)" =~ " " ]]; then echo This script relies on Miniconda which can not be silently installed under a path with spaces. && exit; fi
 
+# deactivate existing env if needed
+conda deactivate 2> /dev/null
+
 # config
 CONDA_ROOT_PREFIX="$(pwd)/installer_files/conda"
 INSTALL_ENV_DIR="$(pwd)/installer_files/env"
@@ -16,4 +19,6 @@ export CUDA_PATH="$INSTALL_ENV_DIR"
 export CUDA_HOME="$CUDA_PATH"
 
 # activate env
-bash --init-file <(echo "source \"$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh\" && conda activate \"$INSTALL_ENV_DIR\"")
+source $CONDA_ROOT_PREFIX/etc/profile.d/conda.sh
+conda activate $INSTALL_ENV_DIR
+exec bash --norc
