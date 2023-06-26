5c09e2a73679c24fe87d5cfcc8732479c0ac6776
Panchovix <panchovixmenn@gmail.com>
2023-06-25 21:05:02 -0400

    Fix variable name

--- modules/exllama.py
+++ modules/exllama.py
@@ -51,7 +51,7 @@ class ExllamaModel:
             config.gpu_peer_fix = True
 
         if shared.args.max_ctx_length:
-            config.max_seq_len = int(shared.args.ctx_length)
+            config.max_seq_len = int(shared.args.max_ctx_length)
 
         if shared.args.compress_pos_emb:
             config.compress_pos_emb = int(shared.args.compress_pos_emb)
