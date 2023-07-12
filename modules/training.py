4be3482df380cea8c673ff7e856ea9a4992a5646
FPHam <fartypantsham@gmail.com>
2023-07-12 13:03:35 -0400

    merge 2

--- modules/training.py
+++ modules/training.py
@@ -265,6 +265,8 @@ def calc_trainable_parameters(model):
         if param.requires_grad:
             trainable_params += num_params
 
+    return trainable_params, all_param
+
 def do_train(lora_name: str, always_override: bool, save_steps: int, micro_batch_size: int, batch_size: int, epochs: int, learning_rate: str, lr_scheduler_type: str, lora_rank: int, lora_alpha: int, lora_dropout: float, cutoff_len: int, dataset: str, eval_dataset: str, format: str, eval_steps: int, raw_text_file: str, overlap_len: int, newline_favor_len: int, higher_rank_limit: bool, warmup_steps: int, optimizer: str, hard_cut_string: str, train_only_after: str, stop_at_loss: float, add_eos_token: bool, min_chars: int, report_to: str):
 
     if shared.args.monkey_patch:
