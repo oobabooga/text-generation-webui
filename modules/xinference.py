66cee95bf654f73d95fbef3a82d2d798130f86da
Bojun-Feng <davidfeng@uchicago.edu>
2023-07-13 17:00:11 +0800

    change import location

--- modules/xinference.py
+++ modules/xinference.py
@@ -3,21 +3,21 @@ Based on
 https://github.com/xorbitsai/inference
 '''
 
+try:
+    from xinference.client import Client
+except ImportError:
+    error_message = "Failed to import module 'xinference'"
+    installation_guide = [
+        "Please make sure 'xinference' is installed. ",
+        "You can visit the original git repo for details:\n",
+        "https://github.com/xorbitsai/inference",
+    ]
+
+    raise ImportError(f"{error_message}\n\n{''.join(installation_guide)}")
 
 class XinferenceModel:
     def __init__(self):
-        try:
-            from xinference.client import Client
-        except ImportError:
-            error_message = "Failed to import module 'xinference'"
-            installation_guide = [
-                "Please make sure 'xinference' is installed. ",
-                "You can visit the original git repo for details:\n",
-                "https://github.com/xorbitsai/inference",
-            ]
-
-            raise ImportError(f"{error_message}\n\n{''.join(installation_guide)}")
-
+        pass
 
     @classmethod
     def from_pretrained(self, model_name, model_uid, endpoint):
