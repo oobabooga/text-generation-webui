8649c264c4d23d1abaa79c810ef540750e8b8286
Bojun-Feng <davidfeng@uchicago.edu>
2023-07-13 16:46:38 +0800

    handle import error

--- modules/xinference.py
+++ modules/xinference.py
@@ -6,12 +6,21 @@ Documentation:
 https://abetlen.github.io/llama-cpp-python/
 '''
 
-from xinference.client import Client
-
 
 class XinferenceModel:
     def __init__(self):
-        pass
+        try:
+            from xinference.client import Client
+        except ImportError:
+            error_message = "Failed to import module 'xinference'"
+            installation_guide = [
+                "Please make sure 'xinference' is installed. ",
+                "You can visit the original git repo for details:\n",
+                "https://github.com/xorbitsai/inference",
+            ]
+
+            raise ImportError(f"{error_message}\n\n{''.join(installation_guide)}")
+
 
     @classmethod
     def from_pretrained(self, model_name, model_uid, endpoint):
