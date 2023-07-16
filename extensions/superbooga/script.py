c85b26dd1e74d5fb5822c7b6063cf6409e47b138
tc <tczhong24@gmail.com>
2023-07-15 18:32:20 -0700

    update plugin to chinese

--- extensions/superbooga/script.py
+++ extensions/superbooga/script.py
@@ -11,13 +11,13 @@ from .chromadb import add_chunks_to_collector, make_collector
 from .download_urls import download_urls
 
 params = {
-    'chunk_count': 5,
-    'chunk_count_initial': 10,
-    'time_weight': 0,
-    'chunk_length': 700,
-    'chunk_separator': '',
-    'strong_cleanup': False,
-    'threads': 4,
+    "chunk_count": 5,
+    "chunk_count_initial": 10,
+    "time_weight": 0,
+    "chunk_length": 700,
+    "chunk_separator": "",
+    "strong_cleanup": False,
+    "threads": 4,
 }
 
 collector = make_collector()
@@ -29,18 +29,26 @@ def feed_data_into_collector(corpus, chunk_len, chunk_sep):
 
     # Defining variables
     chunk_len = int(chunk_len)
-    chunk_sep = chunk_sep.replace(r'\n', '\n')
-    cumulative = ''
+    chunk_sep = chunk_sep.replace(r"\n", "\n")
+    cumulative = ""
 
     # Breaking the data into chunks and adding those to the db
     cumulative += "Breaking the input dataset...\n\n"
     yield cumulative
     if chunk_sep:
         data_chunks = corpus.split(chunk_sep)
-        data_chunks = [[data_chunk[i:i + chunk_len] for i in range(0, len(data_chunk), chunk_len)] for data_chunk in data_chunks]
+        data_chunks = [
+            [
+                data_chunk[i : i + chunk_len]
+                for i in range(0, len(data_chunk), chunk_len)
+            ]
+            for data_chunk in data_chunks
+        ]
         data_chunks = [x for y in data_chunks for x in y]
     else:
-        data_chunks = [corpus[i:i + chunk_len] for i in range(0, len(corpus), chunk_len)]
+        data_chunks = [
+            corpus[i : i + chunk_len] for i in range(0, len(corpus), chunk_len)
+        ]
 
     cumulative += f"{len(data_chunks)} chunks have been found.\n\nAdding the chunks to the database...\n\n"
     yield cumulative
@@ -50,23 +58,23 @@ def feed_data_into_collector(corpus, chunk_len, chunk_sep):
 
 
 def feed_file_into_collector(file, chunk_len, chunk_sep):
-    yield 'Reading the input dataset...\n\n'
-    text = file.decode('utf-8')
+    yield "Reading the input dataset...\n\n"
+    text = file.decode("utf-8")
     for i in feed_data_into_collector(text, chunk_len, chunk_sep):
         yield i
 
 
 def feed_url_into_collector(urls, chunk_len, chunk_sep, strong_cleanup, threads):
-    all_text = ''
-    cumulative = ''
+    all_text = ""
+    cumulative = ""
 
-    urls = urls.strip().split('\n')
-    cumulative += f'Loading {len(urls)} URLs with {threads} threads...\n\n'
+    urls = urls.strip().split("\n")
+    cumulative += f"Loading {len(urls)} URLs with {threads} threads...\n\n"
     yield cumulative
     for update, contents in download_urls(urls, threads=threads):
         yield cumulative + update
 
-    cumulative += 'Processing the HTML sources...'
+    cumulative += "Processing the HTML sources..."
     yield cumulative
     for content in contents:
         soup = BeautifulSoup(content, features="html.parser")
@@ -77,7 +85,7 @@ def feed_url_into_collector(urls, chunk_len, chunk_sep, strong_cleanup, threads)
         if strong_cleanup:
             strings = [s for s in strings if re.search("[A-Za-z] ", s)]
 
-        text = '\n'.join([s.strip() for s in strings])
+        text = "\n".join([s.strip() for s in strings])
         all_text += text
 
     for i in feed_data_into_collector(all_text, chunk_len, chunk_sep):
@@ -86,48 +94,66 @@ def feed_url_into_collector(urls, chunk_len, chunk_sep, strong_cleanup, threads)
 
 def apply_settings(chunk_count, chunk_count_initial, time_weight):
     global params
-    params['chunk_count'] = int(chunk_count)
-    params['chunk_count_initial'] = int(chunk_count_initial)
-    params['time_weight'] = time_weight
-    settings_to_display = {k: params[k] for k in params if k in ['chunk_count', 'chunk_count_initial', 'time_weight']}
+    params["chunk_count"] = int(chunk_count)
+    params["chunk_count_initial"] = int(chunk_count_initial)
+    params["time_weight"] = time_weight
+    settings_to_display = {
+        k: params[k]
+        for k in params
+        if k in ["chunk_count", "chunk_count_initial", "time_weight"]
+    }
     yield f"The following settings are now active: {str(settings_to_display)}"
 
 
 def custom_generate_chat_prompt(user_input, state, **kwargs):
     global chat_collector
 
-    if state['mode'] == 'instruct':
-        results = collector.get_sorted(user_input, n_results=params['chunk_count'])
-        additional_context = '\nYour reply should be based on the context below:\n\n' + '\n'.join(results)
+    if state["mode"] == "instruct":
+        results = collector.get_sorted(user_input, n_results=params["chunk_count"])
+        additional_context = (
+            "\nYour reply should be based on the context below:\n\n"
+            + "\n".join(results)
+        )
         user_input += additional_context
     else:
 
         def make_single_exchange(id_):
-            output = ''
+            output = ""
             output += f"{state['name1']}: {shared.history['internal'][id_][0]}\n"
             output += f"{state['name2']}: {shared.history['internal'][id_][1]}\n"
             return output
 
-        if len(shared.history['internal']) > params['chunk_count'] and user_input != '':
+        if len(shared.history["internal"]) > params["chunk_count"] and user_input != "":
             chunks = []
-            hist_size = len(shared.history['internal'])
-            for i in range(hist_size-1):
+            hist_size = len(shared.history["internal"])
+            for i in range(hist_size - 1):
                 chunks.append(make_single_exchange(i))
 
             add_chunks_to_collector(chunks, chat_collector)
-            query = '\n'.join(shared.history['internal'][-1] + [user_input])
+            query = "\n".join(shared.history["internal"][-1] + [user_input])
             try:
-                best_ids = chat_collector.get_ids_sorted(query, n_results=params['chunk_count'], n_initial=params['chunk_count_initial'], time_weight=params['time_weight'])
-                additional_context = '\n'
+                best_ids = chat_collector.get_ids_sorted(
+                    query,
+                    n_results=params["chunk_count"],
+                    n_initial=params["chunk_count_initial"],
+                    time_weight=params["time_weight"],
+                )
+                additional_context = "\n"
                 for id_ in best_ids:
-                    if shared.history['internal'][id_][0] != '<|BEGIN-VISIBLE-CHAT|>':
+                    if shared.history["internal"][id_][0] != "<|BEGIN-VISIBLE-CHAT|>":
                         additional_context += make_single_exchange(id_)
 
-                logger.warning(f'Adding the following new context:\n{additional_context}')
-                state['context'] = state['context'].strip() + '\n' + additional_context
-                kwargs['history'] = {
-                    'internal': [shared.history['internal'][i] for i in range(hist_size) if i not in best_ids],
-                    'visible': ''
+                logger.warning(
+                    f"Adding the following new context:\n{additional_context}"
+                )
+                state["context"] = state["context"].strip() + "\n" + additional_context
+                kwargs["history"] = {
+                    "internal": [
+                        shared.history["internal"][i]
+                        for i in range(hist_size)
+                        if i not in best_ids
+                    ],
+                    "visible": "",
                 }
             except RuntimeError:
                 logger.error("Couldn't query the database, moving on...")
@@ -136,8 +162,8 @@ def custom_generate_chat_prompt(user_input, state, **kwargs):
 
 
 def remove_special_tokens(string):
-    pattern = r'(<\|begin-user-input\|>|<\|end-user-input\|>|<\|injection-point\|>)'
-    return re.sub(pattern, '', string)
+    pattern = r"(<\|begin-user-input\|>|<\|end-user-input\|>|<\|injection-point\|>)"
+    return re.sub(pattern, "", string)
 
 
 def input_modifier(string):
@@ -151,63 +177,65 @@ def input_modifier(string):
         user_input = match.group(1).strip()
 
         # Get the most similar chunks
-        results = collector.get_sorted(user_input, n_results=params['chunk_count'])
+        results = collector.get_sorted(user_input, n_results=params["chunk_count"])
 
         # Make the injection
-        string = string.replace('<|injection-point|>', '\n'.join(results))
+        string = string.replace("<|injection-point|>", "\n".join(results))
 
     return remove_special_tokens(string)
 
 
 def ui():
     with gr.Accordion("Click for more information...", open=False):
-        gr.Markdown(textwrap.dedent("""
+        gr.Markdown(
+            textwrap.dedent(
+                """
 
-        ## About
+        ## 关于
 
-        This extension takes a dataset as input, breaks it into chunks, and adds the result to a local/offline Chroma database.
+        该扩展将数据集作为输入，将其分割成块，并将结果添加到本地/离线 Chroma 数据库中。
 
-        The database is then queried during inference time to get the excerpts that are closest to your input. The idea is to create an arbitrarily large pseudo context.
+        然后在推理过程中查询该数据库，以获得与输入内容最接近的摘录。我们的想法是创建一个任意大的伪上下文。
 
-        The core methodology was developed and contributed by kaiokendev, who is working on improvements to the method in this repository: https://github.com/kaiokendev/superbig
+        该核心方法由kaiokendev开发和贡献，他正在该资源库中对该方法进行改进。: https://github.com/kaiokendev/superbig
 
-        ## Data input
+        ## 数据输入
+        
+        首先在下面的界面中输入一些数据，然后点击 "加载数据"。
 
-        Start by entering some data in the interface below and then clicking on "Load data".
+        每次加载新数据时，旧数据块将被丢弃。
+        
+        ## 聊天模式
 
-        Each time you load some new data, the old chunks are discarded.
+        ### Instruct Mode
 
-        ## Chat mode
-
-        #### Instruct
-
-        On each turn, the chunks will be compared to your current input and the most relevant matches will be appended to the input in the following format:
+        在每一轮中，数据块将与您当前的输入进行比较，最匹配的数据块将以以下格式添加到输入中：
 
         ```
-        Consider the excerpts below as additional context:
+        请将以下摘录作为额外的上下文：
         ...
         ```
 
-        The injection doesn't make it into the chat history. It is only used in the current generation.
+        注入不会进入聊天历史。它只用于当前生成。
 
-        #### Regular chat
+        ### Chat Mode
 
-        The chunks from the external data sources are ignored, and the chroma database is built based on the chat history instead. The most relevant past exchanges relative to the present input are added to the context string. This way, the extension acts as a long term memory.
+        来自外部数据源的数据块将被忽略，chroma数据库将根据聊天历史建立。相对于当前输入，最相关的过去交流被添加到上下文字符串中。通过这种方式，扩展功能可作为长期记忆。
 
-        ## Notebook/default modes
+        ## 笔记本/默认模式
 
-        Your question must be manually specified between `<|begin-user-input|>` and `<|end-user-input|>` tags, and the injection point must be specified with `<|injection-point|>`.
+        您的问题必须在"<|begin-user-input|>"和"<|end-user-input|>"标签之间手动指定，注入点必须用"<|injection-point|>"指定。
 
-        The special tokens mentioned above (`<|begin-user-input|>`, `<|end-user-input|>`, and `<|injection-point|>`) are removed in the background before the text generation begins.
+        上面提到的特殊标记（`<|begin-user-input|>`, `<|end-user-input|>`和`<|injection-point|>`）会在文本生成开始前在后台被移除。
 
-        Here is an example in Vicuna 1.1 format:
+        下面是一个Vicuna 1.1格式的示例：
 
         ```
-        A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
+        一个好奇的用户和一个人工智能助手之间的聊天。人工智能助手对用户的问题给出了有用、详细和礼貌的回答。
 
-        USER:
+        用户：
 
-        <|begin-user-input|>
+         <|begin-user-input|>
         What datasets are mentioned in the text below?
         <|end-user-input|>
 
@@ -216,42 +244,99 @@ def ui():
         ASSISTANT:
         ```
 
-        ⚠️  For best results, make sure to remove the spaces and new line characters after `ASSISTANT:`.
+        ⚠️ 为获得最佳效果，请确保删除`ASSISTANT:`后的空格和新行字符。
 
-        *This extension is currently experimental and under development.*
+        *此扩展目前是试验性的，正在开发中。
 
-        """))
+        """
+            )
+        )
 
     with gr.Row():
         with gr.Column(min_width=600):
             with gr.Tab("Text input"):
-                data_input = gr.Textbox(lines=20, label='Input data')
-                update_data = gr.Button('Load data')
+                data_input = gr.Textbox(lines=20, label="Input data")
+                update_data = gr.Button("Load data")
 
             with gr.Tab("URL input"):
-                url_input = gr.Textbox(lines=10, label='Input URLs', info='Enter one or more URLs separated by newline characters.')
-                strong_cleanup = gr.Checkbox(value=params['strong_cleanup'], label='Strong cleanup', info='Only keeps html elements that look like long-form text.')
-                threads = gr.Number(value=params['threads'], label='Threads', info='The number of threads to use while downloading the URLs.', precision=0)
-                update_url = gr.Button('Load data')
+                url_input = gr.Textbox(
+                    lines=10,
+                    label="Input URLs",
+                    info="输入一个或多个以换行符分隔的 URL。",
+                )
+                strong_cleanup = gr.Checkbox(
+                    value=params["strong_cleanup"],
+                    label="Strong cleanup",
+                    info="只保留看起来像长文本的html元素。",
+                )
+                threads = gr.Number(
+                    value=params["threads"],
+                    label="线程",
+                    info="下载URL时使用的线程数。",
+                    precision=0,
+                )
+                update_url = gr.Button("加载数据")
 
-            with gr.Tab("File input"):
-                file_input = gr.File(label='Input file', type='binary')
-                update_file = gr.Button('Load data')
+            with gr.Tab("文件上传"):
+                file_input = gr.File(label="Input file", type="binary")
+                update_file = gr.Button("加载数据")
 
-            with gr.Tab("Generation settings"):
-                chunk_count = gr.Number(value=params['chunk_count'], label='Chunk count', info='The number of closest-matching chunks to include in the prompt.')
-                gr.Markdown('Time weighting (optional, used in to make recently added chunks more likely to appear)')
-                time_weight = gr.Slider(0, 1, value=params['time_weight'], label='Time weight', info='Defines the strength of the time weighting. 0 = no time weighting.')
-                chunk_count_initial = gr.Number(value=params['chunk_count_initial'], label='Initial chunk count', info='The number of closest-matching chunks retrieved for time weight reordering in chat mode. This should be >= chunk count. -1 = All chunks are retrieved. Only used if time_weight > 0.')
+            with gr.Tab("生成设置"):
+                chunk_count = gr.Number(
+                    value=params["chunk_count"],
+                    label="Chunk count",
+                    info="在提示中包含的最接近匹配块的数量。",
+                )
+                gr.Markdown("时间加权（可选，用于使最近添加的块更有可能出现）)")
+                time_weight = gr.Slider(
+                    0,
+                    1,
+                    value=params["time_weight"],
+                    label="Time weight",
+                    info="定义时间加权的强度。0 = 无时间加权。",
+                )
+                chunk_count_initial = gr.Number(
+                    value=params["chunk_count_initial"],
+                    label="Initial chunk count",
+                    info="在聊天模式下为时间权重重排序而检索的最接近匹配的数据块数量。该值应>=数据块数量。-1 = 提取所有的数据块。仅当time_weight > 0时使用。",
+                )
 
-                update_settings = gr.Button('Apply changes')
+                update_settings = gr.Button("Apply changes")
 
-            chunk_len = gr.Number(value=params['chunk_length'], label='Chunk length', info='In characters, not tokens. This value is used when you click on "Load data".')
-            chunk_sep = gr.Textbox(value=params['chunk_separator'], label='Chunk separator', info='Used to manually split chunks. Manually split chunks longer than chunk length are split again. This value is used when you click on "Load data".')
+            chunk_len = gr.Number(
+                value=params["chunk_length"],
+                label="Chunk length",
+                info='以字符为单位。点击 "加载数据 "时使用该值。".',
+            )
+            chunk_sep = gr.Textbox(
+                value=params["chunk_separator"],
+                label="Chunk separator",
+                info='用于手动分割数据块。手动分割的数据块长度大于数据块长度时会再次分割。点击 "加载数据 "时使用该值。',
+            )
         with gr.Column():
             last_updated = gr.Markdown()
 
-    update_data.click(feed_data_into_collector, [data_input, chunk_len, chunk_sep], last_updated, show_progress=False)
-    update_url.click(feed_url_into_collector, [url_input, chunk_len, chunk_sep, strong_cleanup, threads], last_updated, show_progress=False)
-    update_file.click(feed_file_into_collector, [file_input, chunk_len, chunk_sep], last_updated, show_progress=False)
-    update_settings.click(apply_settings, [chunk_count, chunk_count_initial, time_weight], last_updated, show_progress=False)
+    update_data.click(
+        feed_data_into_collector,
+        [data_input, chunk_len, chunk_sep],
+        last_updated,
+        show_progress=False,
+    )
+    update_url.click(
+        feed_url_into_collector,
+        [url_input, chunk_len, chunk_sep, strong_cleanup, threads],
+        last_updated,
+        show_progress=False,
+    )
+    update_file.click(
+        feed_file_into_collector,
+        [file_input, chunk_len, chunk_sep],
+        last_updated,
+        show_progress=False,
+    )
+    update_settings.click(
+        apply_settings,
+        [chunk_count, chunk_count_initial, time_weight],
+        last_updated,
+        show_progress=False,
+    )
--- extensions/whisper_stt/script.py
+++ extensions/whisper_stt/script.py
@@ -3,10 +3,7 @@ import speech_recognition as sr
 
 from modules import shared
 
-input_hijack = {
-    'state': False,
-    'value': ["", ""]
-}
+input_hijack = {"state": False, "value": ["", ""]}
 
 
 def do_stt(audio):
@@ -17,7 +14,9 @@ def do_stt(audio):
     audio_data = sr.AudioData(sample_rate=audio[0], frame_data=audio[1], sample_width=4)
 
     try:
-        transcription = r.recognize_whisper(audio_data, language="english", model="base.en")
+        transcription = r.recognize_whisper(
+            audio_data, language="english", model="base"
+        )
     except sr.UnknownValueError:
         print("Whisper could not understand audio")
     except sr.RequestError as e:
@@ -40,8 +39,15 @@ def auto_transcribe(audio, auto_submit):
 def ui():
     with gr.Row():
         audio = gr.Audio(source="microphone")
-        auto_submit = gr.Checkbox(label='Submit the transcribed audio automatically', value=True)
+        auto_submit = gr.Checkbox(
+            label="Submit the transcribed audio automatically", value=True
+        )
 
     audio.change(
-        auto_transcribe, [audio, auto_submit], [shared.gradio['textbox'], audio]).then(
-        None, auto_submit, None, _js="(check) => {if (check) { document.getElementById('Generate').click() }}")
+        auto_transcribe, [audio, auto_submit], [shared.gradio["textbox"], audio]
+    ).then(
+        None,
+        auto_submit,
+        None,
+        _js="(check) => {if (check) { document.getElementById('Generate').click() }}",
+    )
