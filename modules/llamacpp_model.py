1141987a0d499a0b59a47f819df4edeb195d54d5
jllllll <3887729+jllllll@users.noreply.github.com>
2023-07-24 09:25:36 -0500

    Add checks for ROCm and unsupported architectures to llama_cpp_cuda loading (#3225)

--- modules/llamacpp_model.py
+++ modules/llamacpp_model.py
@@ -1,11 +1,3 @@
-'''
-Based on
-https://github.com/abetlen/llama-cpp-python
-
-Documentation:
-https://abetlen.github.io/llama-cpp-python/
-'''
-
 import re
 from functools import partial
 
@@ -15,8 +7,11 @@ from modules import shared
 from modules.callbacks import Iteratorize
 from modules.logging_colors import logger
 
-if torch.cuda.is_available():
-    from llama_cpp_cuda import Llama, LlamaCache, LogitsProcessorList
+if torch.cuda.is_available() and not torch.version.hip:
+    try:
+        from llama_cpp_cuda import Llama, LlamaCache, LogitsProcessorList
+    except:
+        from llama_cpp import Llama, LlamaCache, LogitsProcessorList
 else:
     from llama_cpp import Llama, LlamaCache, LogitsProcessorList
 
