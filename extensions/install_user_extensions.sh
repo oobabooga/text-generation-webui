741ab3927236e01e459a9f60b61b41b011171e48
Sam McLeod <sammcj@users.noreply.github.com>
2023-08-13 13:39:54 +1000

    minor updates

--- extensions/install_user_extensions.sh
+++ extensions/install_user_extensions.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
-# A simple script that you can call to install any extensions that have been added with a requirements.txt file in their subdirectory.
+# This is basically like an rsync but without needing to install rsync
 
 USERPKGS=/app/venv_user/lib/python3.10/site-packages
 VENV=/app/venv/lib/python3.10/site-packages
+EXTENSIONS=/app/extensions
+export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
-source /app/venv/bin/activate
+/app/venv/bin/activate
 
 # If the directory /app/venv_user exists
 if [ -d "/app/venv_user" ]; then
 
-  echo "Copying any missing files from ${USERPKGS}/ to ${VENV}/"
-  cp -R -n "${USERPKGS}/"* "${VENV}/"
+  echo "Copying any missing files from the cache ${USERPKGS}/ to ${VENV}/"
+  cp -anR "${USERPKGS}/"* "${VENV}/"
 
-  for d in "${USERPKGS}"/*/; do
-    cd "${VENV}/$d" || exit
-    git pull || true
+  for d in "${EXTENSIONS}"/*/; do
+    cd "$d" || exit
+    if [ -d ".git" ]; then
+      echo "Updating extension $d"
+      git pull || true
+    fi
     if [ -f "requirements.txt" ]; then
-      pip install -r "${VENV}/$d/requirements.txt" || true
+      pip install -r "requirements.txt" || true
       cd - || exit
     fi
   done
 fi
+
+# Copy any missing files from /app/venv to /app/user_venv, without overwriting any existing files
+echo "Caching files in ${VENV}/ by copying to ${USERPKGS}/"
+cp -anR "${VENV}/"* "${USERPKGS}/"
