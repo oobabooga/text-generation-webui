70df8dfa2e4f3bf1744832d7cd0c90cde6703082
Ricardo Pinto <3410904+ricardopinto@users.noreply.github.com>
2023-08-27 19:18:58 +0000

    Checking if state has keys before using them

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -241,7 +241,8 @@ def apply_stopping_strings(reply, all_stop_strings):
 def generate_reply_HF(question, original_question, seed, state, stopping_strings=None, is_chat=False):
     generate_params = {}
     for k in ['max_new_tokens', 'do_sample', 'temperature', 'top_p', 'typical_p', 'repetition_penalty', 'repetition_penalty_range', 'encoder_repetition_penalty', 'top_k', 'min_length', 'no_repeat_ngram_size', 'num_beams', 'penalty_alpha', 'length_penalty', 'early_stopping', 'tfs', 'top_a', 'mirostat_mode', 'mirostat_tau', 'mirostat_eta', 'guidance_scale']:
-        generate_params[k] = state[k]
+        if k in state:
+            generate_params[k] = state[k]
 
     if state['negative_prompt'] != '':
         generate_params['negative_prompt_ids'] = encode(state['negative_prompt'])
