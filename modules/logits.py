85ffe1688269f9f443745fb9570acb9b5cb9ca11
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-09-17 06:36:06 -0700

    Minor changes

--- modules/logits.py
+++ modules/logits.py
@@ -25,7 +25,8 @@ def get_next_logits(prompt, state, use_samplers, previous):
             # sampling is all done in c for exllama, so it is really hard to hijack
             # it should be possible to hijack llamacpp sampler by hijacking all their sampling methods,
             # but it is not implemented yet
-            return 'Error: Sampler hijacking is not supported non-Huggingface loaders', previous
+            return 'Error: Sampler hijacking is not supported non-Huggingface loaders. Please disable the "Use samplers" option.', previous
+
         state['max_new_tokens'] = 1
         state['auto_max_new_tokens'] = False
         for _ in generate_reply(prompt, state):
@@ -49,12 +50,13 @@ def get_next_logits(prompt, state, use_samplers, previous):
     topk_values = [f"{float(i):.5f}" for i in topk_values]
     if is_non_hf_exllamav1 or is_non_hf_llamacpp:
         topk_indices = [i.expand((1, 1)) for i in topk_indices]
+
     tokens = [shared.tokenizer.decode(i) for i in topk_indices]
     if is_non_hf_llamacpp:
-        tokens = [i.decode('utf-8') for i in tokens] # llamacpp returns bytes, not str
+        tokens = [i.decode('utf-8') for i in tokens]  # llamacpp returns bytes, not str
 
     output = ''
     for row in list(zip(topk_values, tokens)):
-        output += f"{row[0]}  -  {row[1]}\n"
+        output += f"{row[0]}  -  {repr(row[1])[1:-1]}\n"
 
     return output, previous
