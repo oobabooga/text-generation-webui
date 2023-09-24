6606db5f5f79f71458897bdb3f4553d0b7ee5b94
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-09-23 20:40:34 -0700

    Fix cpuinfo install

--- one_click.py
+++ one_click.py
@@ -157,7 +157,7 @@ def install_webui():
         install_pytorch = "python -m pip install torch==2.0.1a0 torchvision==0.15.2a0 intel_extension_for_pytorch==2.0.110+xpu -f https://developer.intel.com/ipex-whl-stable-xpu"
 
     # Install Git and then Pytorch
-    run_cmd(f"{install_git} && {install_pytorch} && python -m pip install cpuinfo", assert_success=True, environment=True)
+    run_cmd(f"{install_git} && {install_pytorch} && python -m pip install py-cpuinfo", assert_success=True, environment=True)
 
     # Install the webui requirements
     update_requirements(initial_installation=True)
