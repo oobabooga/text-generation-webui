8ad402f6b3ebe23e472c2c78e410bb8360220380
jsboige <jsboige@gmail.com>
2023-09-26 16:26:13 +0200

    restoring env. variable support for cmd args

--- one_click.py
+++ one_click.py
@@ -15,12 +15,18 @@ conda_env_path = os.path.join(script_dir, "installer_files", "env")
 # os.environ["HCC_AMDGPU_TARGET"] = 'gfx1030'
 
 # Command-line flags
-cmd_flags_path = os.path.join(script_dir, "CMD_FLAGS.txt")
-if os.path.exists(cmd_flags_path):
-    with open(cmd_flags_path, 'r') as f:
-        CMD_FLAGS = ' '.join(line.strip() for line in f if line.strip() and not line.strip().startswith('#'))
+if "OOBABOOGA_FLAGS" in os.environ:
+    CMD_FLAGS = os.environ["OOBABOOGA_FLAGS"]
+    print("The following flags have been taken from the environment variable 'OOBABOOGA_FLAGS':")
+    print(CMD_FLAGS)
+    print("To use the CMD_FLAGS Inside CMD_FLAGS.txt, unset 'OOBABOOGA_FLAGS'.\n")
 else:
-    CMD_FLAGS = ''
+    cmd_flags_path = os.path.join(script_dir, "CMD_FLAGS.txt")
+    if os.path.exists(cmd_flags_path):
+        with open(cmd_flags_path, 'r') as f:
+            CMD_FLAGS = ' '.join(line.strip() for line in f if line.strip() and not line.strip().startswith('#'))
+    else:
+        CMD_FLAGS = ''
 
 
 def is_linux():
