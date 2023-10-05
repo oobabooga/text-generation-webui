68f90f788f7e3033dab50c745b66493b33b55b9c
turboderp <turboderp@users.noreply.github.com>
2023-10-05 18:36:25 +0200

    Allow configuring BOS token for ExLlamaV2Model

--- modules/loaders.py
+++ modules/loaders.py
@@ -218,6 +218,7 @@ loaders_samplers = {
         'repetition_penalty_range',
         'seed',
         'ban_eos_token',
+        'add_bos_token',
         'custom_token_bans',
         'auto_max_new_tokens',
     },
