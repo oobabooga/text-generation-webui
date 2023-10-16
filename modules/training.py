71cac7a1b20d61c8788702655ca617e05fcfacf7
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-10-15 21:56:40 -0700

    Increase the height of the evaluation table

--- modules/training.py
+++ modules/training.py
@@ -156,7 +156,7 @@ def create_ui():
                 with gr.Column():
                     evaluation_log = gr.Markdown(value='')
 
-            evaluation_table = gr.Dataframe(value=generate_markdown_table(), interactive=True)
+            evaluation_table = gr.Dataframe(value=generate_markdown_table(), interactive=True, height=20000, elem_id='evaluation-table')
             with gr.Row():
                 save_comments = gr.Button('Save comments', elem_classes="small-button", interactive=not mu)
                 refresh_table = gr.Button('Refresh the table', elem_classes="small-button", interactive=not mu)
