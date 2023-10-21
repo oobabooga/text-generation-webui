991a9ff989645ae5649cbc21dfd71d00f272ee3e
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-10-20 17:52:55 -0700

    CUDA_118 environment variable

--- one_click.py
+++ one_click.py
@@ -170,15 +170,18 @@ def install_webui():
     # Find the proper Pytorch installation command
     install_git = "conda install -y -k ninja git"
     install_pytorch = "python -m pip install torch torchvision torchaudio"
-    use_cuda118 = "N"
 
+    use_cuda118 = "N"
     if any((is_windows(), is_linux())) and choice == "A":
-        # Ask for cuda version if using Nvidia
-        print("Would you like to use CUDA 11.8 instead of 12.1? This is only necessary for older GPUs like Kepler. For Ampere GPUs, say \"N\".")
-        use_cuda118 = input("Input (Y/N)> ").upper().strip('"').strip()
-        while use_cuda118 not in 'YN':
-            print("Invalid choice. Please try again.")
-            use_cuda118 = input("Input> ").upper().strip('"').strip()
+        if "CUDA_118" in os.environ:
+            use_cuda118 = os.environ.get("USE_CUDA118", "").lower() in ("yes", "y", "trye", "1", "t", "on")
+        else:
+            # Ask for CUDA version if using NVIDIA
+            print("\nWould you like to use CUDA 11.8 instead of 12.1? This is only necessary for older GPUs like Kepler.\nIf unsure, say \"N\".\n")
+            use_cuda118 = input("Input (Y/N)> ").upper().strip('"').strip()
+            while use_cuda118 not in 'YN':
+                print("Invalid choice. Please try again.")
+                use_cuda118 = input("Input> ").upper().strip('"').strip()
 
         install_pytorch = f"python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/{'cu121' if use_cuda118 == 'N' else 'cu118'}"
     elif not is_macos() and choice == "B":
