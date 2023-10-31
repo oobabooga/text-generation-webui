c389ef0f6dbe68997e1daaee3042bee56bc44583
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-10-31 16:28:32 -0700

    Revert "Long shot"
    This reverts commit 5d0a6f452e5d385f32d96a58200515adff7d29f5.

--- extensions/openai/fastapi (broken).py
+++ extensions/openai/fastapi (broken).py
@@ -9,7 +9,6 @@ from sse_starlette import EventSourceResponse
 
 app = FastAPI()
 
-
 class CompletionRequest(BaseModel):
     model: str | None = None
     prompt: str | List[str]
@@ -155,7 +154,7 @@ async def openai_chat_completions(request_data: ChatCompletionRequest):
 async def model_response():
     object = ModelObject(id="")
     response = ModelResponse(
-        data=[to_dict(object)]
+        data = [to_dict(object)]
     )
 
     return JSONResponse(content=to_dict(response))
