7f9c1cbb30b13a313923d994b34ce175c522f9d8
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-11-03 08:25:22 -0700

    Change min_p default to 0.0

--- modules/sampler_hijack.py
+++ modules/sampler_hijack.py
@@ -252,7 +252,7 @@ def get_logits_processor_patch(self, **kwargs):
 
 def generation_config_init_patch(self, **kwargs):
     self.__init___old(**kwargs)
-    self.min_p = kwargs.pop("min_p", 1.0)
+    self.min_p = kwargs.pop("min_p", 0.0)
     self.tfs = kwargs.pop("tfs", 1.0)
     self.top_a = kwargs.pop("top_a", 0.0)
     self.mirostat_mode = kwargs.pop("mirostat_mode", 0)
