0ed6a17ed4a805538e16e69d752b037c3c7b89f2
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-11-06 07:17:49 -0800

    Update warning

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -7,7 +7,7 @@ from modules.logging_colors import logger
 
 
 def setup():
-    logger.warning("The current API is deprecated and will be replaced with the OpenAI compatible API on November xxth. To test the new API, use \"--extensions openai\" instead of \"--api\".")
+    logger.warning("\nThe current API is deprecated and will be replaced with the OpenAI compatible API on November 13th.\nTo test the new API, use \"--extensions openai\" instead of \"--api\".\nFor documentation on the new API, consult:\nhttps://github.com/oobabooga/text-generation-webui/wiki/12-%E2%80%90-OpenAI-API")
     blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
     if shared.args.public_api:
         time.sleep(5)
