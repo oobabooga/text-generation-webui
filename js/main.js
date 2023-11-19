2efc71de9ef2afa3d729060d5dea0e5d2ff340e4
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-11-18 21:04:49 -0800

    Clean up

--- js/main.js
+++ js/main.js
@@ -373,12 +373,4 @@ function toggleBigPicture() {
   }
 }
 
-function updateBigPicture() {
-  var existingElement = document.querySelector('.bigProfilePicture');
-  if (existingElement) {
-    var timestamp = new Date().getTime();
-    existingElement.src = "/file/cache/pfp_character.png?time=" + timestamp;
-  }
-}
-
 showBigPicture();
