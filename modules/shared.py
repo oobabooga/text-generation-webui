073392ef42756064cbf245813d5d22c9830b9edd
oobabooga <112222186+oobabooga@users.noreply.github.com>
2023-12-07 14:52:16 -0800

    Change max_new_tokens default to 512

--- modules/shared.py
+++ modules/shared.py
@@ -36,7 +36,7 @@ settings = {
     'prompt-default': 'QA',
     'prompt-notebook': 'QA',
     'preset': 'simple-1',
-    'max_new_tokens': 200,
+    'max_new_tokens': 512,
     'max_new_tokens_min': 1,
     'max_new_tokens_max': 4096,
     'negative_prompt': '',
