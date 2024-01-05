9e86bea8e92e77fee0521f5454e14185055c7e92
oobabooga <112222186+oobabooga@users.noreply.github.com>
2024-01-04 18:51:52 -0800

    Use requirements_cpu.txt for intel

--- one_click.py
+++ one_click.py
@@ -294,12 +294,12 @@ def update_requirements(initial_installation=False):
     is_cuda118 = '+cu118' in torver  # 2.1.0+cu118
     is_cuda117 = '+cu117' in torver  # 2.0.1+cu117
     is_rocm = '+rocm' in torver  # 2.0.1+rocm5.4.2
-    # is_intel = '+cxx11' in torver  # 2.0.1a0+cxx11.abi
+    is_intel = '+cxx11' in torver  # 2.0.1a0+cxx11.abi
     is_cpu = '+cpu' in torver  # 2.0.1+cpu
 
     if is_rocm:
         base_requirements = "requirements_amd" + ("_noavx2" if not cpu_has_avx2() else "") + ".txt"
-    elif is_cpu:
+    elif is_cpu or is_intel:
         base_requirements = "requirements_cpu_only" + ("_noavx2" if not cpu_has_avx2() else "") + ".txt"
     elif is_macos():
         base_requirements = "requirements_apple_" + ("intel" if is_x86_64() else "silicon") + ".txt"
