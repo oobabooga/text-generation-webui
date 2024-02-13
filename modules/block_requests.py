54d9faef228d5be81ce2ea706afa71313b283980
oobabooga <112222186+oobabooga@users.noreply.github.com>
2024-02-12 17:21:16 -0800

    Revert unnecessary changes

--- modules/block_requests.py
+++ modules/block_requests.py
@@ -40,7 +40,6 @@ def my_open(*args, **kwargs):
         with original_open(*args, **kwargs) as f:
             file_contents = f.read()
 
-        file_contents = file_contents.replace(b'</head>', b'<link rel="stylesheet" type="text/css" href="file/css/NotoSans/stylesheet.css"></head>')
         file_contents = file_contents.replace(b'\t\t<script\n\t\t\tsrc="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.9/iframeResizer.contentWindow.min.js"\n\t\t\tasync\n\t\t></script>', b'')
         file_contents = file_contents.replace(b'cdnjs.cloudflare.com', b'127.0.0.1')
 
