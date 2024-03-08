37ab9378df253303e500c7095ae9a35eb4ce3c73
oobabooga <112222186+oobabooga@users.noreply.github.com>
2024-03-08 11:48:55 -0800

    Revert "AQLM support (experimental)"
    This reverts commit 71737bed8f79dac0bf9dd0f9c097081e06c311c2.

--- modules/models.py
+++ modules/models.py
@@ -128,9 +128,9 @@ def load_tokenizer(model_name, model):
 def huggingface_loader(model_name):
     path_to_model = Path(f'{shared.args.model_dir}/{model_name}')
     params = {
-        # 'low_cpu_mem_usage': True,
+        'low_cpu_mem_usage': True,
         'trust_remote_code': shared.args.trust_remote_code,
-        'torch_dtype': torch.bfloat16 if shared.args.bf16 else "auto",
+        'torch_dtype': torch.bfloat16 if shared.args.bf16 else torch.float16,
         'use_safetensors': True if shared.args.force_safetensors else None
     }
 
@@ -150,7 +150,6 @@ def huggingface_loader(model_name):
 
     # Load the model in simple 16-bit mode by default
     if not any([shared.args.cpu, shared.args.load_in_8bit, shared.args.load_in_4bit, shared.args.auto_devices, shared.args.disk, shared.args.deepspeed, shared.args.gpu_memory is not None, shared.args.cpu_memory is not None, shared.args.compress_pos_emb > 1, shared.args.alpha_value > 1, shared.args.disable_exllama, shared.args.disable_exllamav2]):
-        print(params)
         model = LoaderClass.from_pretrained(path_to_model, **params)
         if torch.backends.mps.is_available():
             device = torch.device('mps')
