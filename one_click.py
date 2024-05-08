2c43e02d8e10a71779c7a32bbf10ea5991801201
Corneliu Zuzu <corneliu.zuzu.p@gmail.com>
2024-05-08 10:55:13 +0300

    Misc: shorthand for enabling remote debugging

--- one_click.py
+++ one_click.py
@@ -420,6 +420,8 @@ def update_requirements(initial_installation=False, pull=True):
 
 def launch_webui():
     run_cmd(f"python server.py {flags}", environment=True)
+    # Switch with the next line for remote debugging (VSCode: Python Debugger: Remote Attach)
+    # run_cmd(f"python -m debugpy --listen :11111 --wait-for-client server.py {flags}", environment=True)
 
 
 if __name__ == "__main__":
