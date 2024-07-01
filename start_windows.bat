9d5e8b7254af1c3c3abbe883dafd6408a9d4db2e
RandoInternetPreson <aaronalai1@gmail.com>
2024-07-01 16:31:13 -0400

    Update start_windows.bat
    I've tested this in Ubuntu, but not windows

--- start_windows.bat
+++ start_windows.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 cd /D "%~dp0"
-
+set ALLOW_RESET=TRUE
 set PATH=%PATH%;%SystemRoot%\system32
 
 echo "%CD%"| findstr /C:" " >nul && echo This script relies on Miniconda which can not be silently installed under a path with spaces. && goto end
