6bab4c2faad9f67154449b850576fed266e6b026
oobabooga <112222186+oobabooga@users.noreply.github.com>
2024-07-26 23:03:53 -0700

    UI: add back single $ for equations

--- js/main.js
+++ js/main.js
@@ -213,6 +213,7 @@ function doSyntaxHighlighting() {
       renderMathInElement(element, {
         delimiters: [
           { left: "$$", right: "$$", display: true },
+          { left: "$", right: "$", display: false },
           { left: "\\(", right: "\\)", display: false },
           { left: "\\[", right: "\\]", display: true },
         ],
