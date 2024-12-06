aac37320a46740ba5820cf011ccfb528493f9d5a
BPplays <andymalbp@gmail.com>
2024-12-06 04:46:16 -0800

    fixed help wording

--- modules/shared.py
+++ modules/shared.py
@@ -195,8 +195,8 @@ group.add_argument('--subpath', type=str, help='Customize the subpath for gradio
 # API
 group = parser.add_argument_group('API')
 group.add_argument('--api', action='store_true', help='Enable the API extension.')
-group.add_argument('--api-disable-ipv6', action='store_true', help='Disables IPv6 for the api')
-group.add_argument('--api-disable-ipv4', action='store_true', help='Disables IPv4 for the api')
+group.add_argument('--api-disable-ipv6', action='store_true', help='Disables IPv6 for the API')
+group.add_argument('--api-disable-ipv4', action='store_true', help='Disables IPv4 for the API')
 group.add_argument('--public-api', action='store_true', help='Create a public URL for the API using Cloudfare.')
 group.add_argument('--public-api-id', type=str, help='Tunnel ID for named Cloudflare Tunnel. Use together with public-api option.', default=None)
 group.add_argument('--api-port', type=int, default=5000, help='The listening port for the API.')
