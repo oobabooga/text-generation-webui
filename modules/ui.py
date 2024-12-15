23825d0a5c829a03133638e1f8b0f02030b9c63d
oobabooga <112222186+oobabooga@users.noreply.github.com>
2024-12-15 15:36:24 -0800

    Make the light theme functional

--- modules/ui.py
+++ modules/ui.py
@@ -58,7 +58,7 @@ if not shared.args.old_colors:
         background_fill_secondary='#eaeaea',
         background_fill_primary='var(--neutral-50)',
         body_background_fill="white",
-        block_background_fill="#f4f4f4",
+        block_background_fill="transparent",
         body_text_color="#333",
         button_secondary_background_fill="#f4f4f4",
         button_secondary_border_color="var(--border-color-primary)",
@@ -82,6 +82,7 @@ if not shared.args.old_colors:
 
         # Shadows and Radius
         checkbox_label_shadow='none',
+        block_shadow='none',
         block_shadow_dark='none',
         button_large_radius='0.375rem',
         button_large_padding='6px 12px',
