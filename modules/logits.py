b0ee1e8863422946ce8921b406994ccf103518ac
oobabooga <112222186+oobabooga@users.noreply.github.com>
2025-04-17 17:15:10 -0700

    Fix showing llama.cpp token probabilities in the UI

--- modules/logits.py
+++ modules/logits.py
@@ -46,7 +46,7 @@ def _get_next_logits(prompt, state, use_samplers, previous, top_logits=25, retur
         if return_dict:
             output = {}
             for entry in logprobs:
-                token = entry['token']
+                token = repr(entry['token'])
                 prob = entry['prob'] if use_samplers else np.exp(entry['logprob'])
                 output[token] = prob
 
@@ -54,7 +54,7 @@ def _get_next_logits(prompt, state, use_samplers, previous, top_logits=25, retur
         else:
             output = ''
             for entry in logprobs:
-                token = entry['token']
+                token = repr(entry['token'])
                 prob = entry['prob'] if use_samplers else np.exp(entry['logprob'])
                 output += f"{prob:.5f}  -  {token}\n"
 
