3e14dc85a943b95d4bb735ab439d573722ce1706
oobabooga <112222186+oobabooga@users.noreply.github.com>
2025-04-25 22:56:22 -0700

    Fix a path

--- modules/utils.py
+++ modules/utils.py
@@ -185,7 +185,7 @@ def get_datasets(path: str, ext: str):
 
 
 def get_available_chat_styles():
-    return sorted(set(('-'.join(k.stem.split('-')[1:]) for k in Path('user_data/css').glob('chat_style*.css'))), key=natural_keys)
+    return sorted(set(('-'.join(k.stem.split('-')[1:]) for k in Path('css').glob('chat_style*.css'))), key=natural_keys)
 
 
 def get_available_grammars():
