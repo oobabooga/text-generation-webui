5676de273752d0070b500f532fab56fe4cbb4a72
marme <marcel.1710@live.de>
2025-05-05 15:43:24 +0200

    dynamically adjust chat message updates/second, remove static 'max_updates_second'

--- modules/shared.py
+++ modules/shared.py
@@ -47,7 +47,6 @@ settings = {
     'max_new_tokens_max': 4096,
     'prompt_lookup_num_tokens': 0,
     'max_tokens_second': 0,
-    'max_updates_second': 12,
     'auto_max_new_tokens': True,
     'ban_eos_token': False,
     'add_bos_token': True,
