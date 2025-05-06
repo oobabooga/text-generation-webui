96d0498c8f53a42875303e195c07985cfac97815
Th-Underscore <therealunderscore16@gmail.com>
2025-05-06 18:11:38 -0400

    Revert accidental edit to JS indentation

--- js/main.js
+++ js/main.js
@@ -192,15 +192,15 @@ function doSyntaxHighlighting() {
           codeBlock.setAttribute("data-highlighted", "true");
         });
 
-            renderMathInElement(messageBody, {
-              delimiters: [
-                { left: "$$", right: "$$", display: true },
-                { left: "$", right: "$", display: false },
-                { left: "\\(", right: "\\)", display: false },
-                { left: "\\[", right: "\\]", display: true },
-              ],
-            });
-        }
+        renderMathInElement(messageBody, {
+          delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+            { left: "\\(", right: "\\)", display: false },
+            { left: "\\[", right: "\\]", display: true },
+          ],
+        });
+      }
     });
 
     observer.observe(targetElement, config);
