efd9c9707b809754b2e467b53bbeb76688768e6d
oobabooga <112222186+oobabooga@users.noreply.github.com>
2025-06-09 20:57:25 -0700

    Fix random seeds being saved to settings.yaml

--- modules/text_generation.py
+++ modules/text_generation.py
@@ -479,6 +479,7 @@ def generate_reply_custom(question, original_question, state, stopping_strings=N
     For models that do not use the transformers library for sampling
     """
 
+    state = copy.deepcopy(state)
     state['seed'] = set_manual_seed(state['seed'])
     t0 = time.time()
     reply = ''
