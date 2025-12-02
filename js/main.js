4d2f151260d6ec0f465c43bcadf8da46c6536028
oobabooga <112222186+oobabooga@users.noreply.github.com>
2025-12-02 07:15:45 -0800

    Revert "Better scrollbar for Image AI > Gallery"
    This reverts commit c3bd1c901d6bbbced037c593d0baacb798c81311.

--- js/main.js
+++ js/main.js
@@ -1105,11 +1105,3 @@ document.fonts.addEventListener("loadingdone", (event) => {
   // Initial call to set the margin based on current state
   updateMargin();
 })();
-
-//------------------------------------------------
-// Better scrollbar for Image AI > Gallery
-//------------------------------------------------
-const element = document.querySelector('#image-history-gallery > :nth-child(2)');
-if (element) {
-  element.classList.add('pretty_scrollbar');
-}
