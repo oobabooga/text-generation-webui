dff8903b03c2b3e46e11c16862f12db0495b3e91
oobabooga <112222186+oobabooga@users.noreply.github.com>
2026-03-16 18:25:54 -0700

    UI: Modernize the Gradio theme

--- modules/ui_session.py
+++ modules/ui_session.py
@@ -17,7 +17,7 @@ def create_ui():
 
             with gr.Column():
                 gr.Markdown("## Extensions & flags")
-                shared.gradio['save_settings'] = gr.Button(f'Save extensions settings to {shared.user_data_dir}/settings.yaml', elem_classes='refresh-button', interactive=not mu)
+                shared.gradio['save_settings'] = gr.Button(f'Save extensions settings to {shared.user_data_dir}/settings.yaml', interactive=not mu)
                 shared.gradio['reset_interface'] = gr.Button("Apply flags/extensions and restart", interactive=not mu)
                 with gr.Row():
                     with gr.Column():
